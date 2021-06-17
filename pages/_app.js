6f0cefbdf3b039af88c8b39b55296008e2fede59
Mariel Ann Filosopo <mariel.filosopo@webriq.services>
2021-06-17 22:49:17 +0800

    initial commit to retain scroll position on preview

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,13 @@ import "../styles/globals.css"
 import React from "react"
 
 function MyApp({ Component, pageProps }) {
+
+  React.useEffect(() =>  {
+    if ("scrollRestoration" in window.history) {
+      window.history.scrollRestoration = "auto"
+    }
+  },[pageProps]);
+
   return <Component {...pageProps} />
 }
 
