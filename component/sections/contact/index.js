e9189432b1344a2d7ba3b79fc59f27ad7f4b2cbb
Mariel Ann Filosopo <mariel.filosopo@webriq.services>
2021-06-23 20:57:04 +0800

    fix how data are shown on page for section

--- component/sections/contact/index.js
+++ component/sections/contact/index.js
@@ -13,6 +13,8 @@ function Contact({ data }) {
     formFields: data?.variants?.[variant]?.form?.fields,
     formId: data?.variants?.[variant]?.form?.id,
     formName: data?.variants?.[variant]?.form?.name,
+    button: data?.variants?.[variant]?.primaryButton,
+    block: data?.variants?.[variant]?.block,
   }
   return <Variant {...props} />
 }
