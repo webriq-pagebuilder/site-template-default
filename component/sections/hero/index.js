809057252ba29b2728a584b373b8cc1fa35b8486
Dorell James <galangdj@gmail.com>
2021-07-13 14:12:18 +0800

    change variants array to object and remove duplicate

--- component/sections/hero/index.js
+++ component/sections/hero/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import dynamic from "next/dynamic";
 
-const Variants = [
-  { variant_a: dynamic(() => import("./variant_a")) },
-  { variant_b: dynamic(() => import("./variant_b")) },
-  { variant_c: dynamic(() => import("./variant_c")) },
-  { variant_d: dynamic(() => import("./variant_d")) },
-  { variant_e: dynamic(() => import("./variant_e")) },
-];
+const Variants = {
+  variant_a: dynamic(() => import("./variant_a")),
+  variant_b: dynamic(() => import("./variant_b")),
+  variant_c: dynamic(() => import("./variant_c")),
+  variant_d: dynamic(() => import("./variant_d")),
+  variant_e: dynamic(() => import("./variant_e")),
+};
 
 function Header({ template, data }) {
   const variant = data?.variants?.variant;
--- component/sections/how_it_works/index.js
+++ component/sections/how_it_works/index.js
@@ -6,7 +6,7 @@ const Variants = {
   variant_b: dynamic(() => import("./variant_b")),
   variant_c: dynamic(() => import("./variant_c")),
   variant_d: dynamic(() => import("./variant_d")),
-  variant_d: dynamic(() => import("./variant_d")),
+  variant_e: dynamic(() => import("./variant_e")),
 };
 
 function HowItWorks({ data }) {
