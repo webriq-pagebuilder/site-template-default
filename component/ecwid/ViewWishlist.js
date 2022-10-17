4813cf8763bb45ecc136dfc7ea6c044efa5d39bc
rabbyrevalde <rabby.revalde@webriq.services>
2022-10-17 23:45:52 +0800

    update UI fixes

--- component/ecwid/ViewWishlist.js
+++ component/ecwid/ViewWishlist.js
@@ -2,21 +2,21 @@ import { useEcwid } from "context/EcwidContext";
 import React from "react";
 
 const ViewWishlist = () => {
-  const ecwid = useEcwid();
-  const isAddingToBag = ecwid?.isAddingToBag;
+   const ecwid = useEcwid();
+   const isAddingToBag = ecwid?.isAddingToBag;
 
-  return (
-    <>
-      <div className="flex flex-row gap-x-4 ">
-        <a
-          href="/wishlist"
-          className="border block w-full text-center font-bold font-heading py-5 px-8 rounded-md uppercase transition duration-200"
-        >
-          View Wishlist
-        </a>
-      </div>
-    </>
-  );
+   return (
+      <>
+         <div className="flex flex-row gap-x-4 ">
+            <a
+               href="/wishlist"
+               className="border block w-full text-center font-bold font-heading py-5 px-8 rounded-md uppercase transition duration-200 hover:border-webriq-darkblue"
+            >
+               View Wishlist
+            </a>
+         </div>
+      </>
+   );
 };
 
 export default ViewWishlist;
