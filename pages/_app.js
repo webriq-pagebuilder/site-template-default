de39ea33330169f5e91f7af46f70c780dabd1224
Dorell James <galangdj@gmail.com>
2022-10-21 16:15:12 +0800

    temp fix for indefinite reload on preview mode with ECWID

--- pages/_app.js
+++ pages/_app.js
@@ -5,14 +5,32 @@ import "swiper/scss";
 import "swiper/scss/navigation";
 import "swiper/scss/pagination";
 import { EcwidContextProvider } from "context/EcwidContext";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
-   return (
+  const { preview } = pageProps;
+  const router = useRouter();
+
+  // patch: cleanup `secret=<secret>&slug=<slug>` when on preview mode as this causes ECWID to refresh indefinitely
+  React.useEffect(() => {
+    if (
+      typeof window !== "undefined" &&
+      preview &&
+      location.search?.includes("secret=") &&
+      location.search?.includes("slug=")
+    ) {
+      router.push(`${window.location.pathname}`);
+    }
+  }, [preview, router]);
+
+  return (
+    <>
       <EcwidContextProvider>
-         <SEO {...pageProps} />
-         <Component {...pageProps} />
+        <SEO {...pageProps} />
+        <Component {...pageProps} />
       </EcwidContextProvider>
-   );
+    </>
+  );
 }
 
 export default React.memo(MyApp);
