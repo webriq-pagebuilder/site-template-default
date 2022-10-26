12ab720cd9fb1012b5a630e74f86ff4d37318904
Mariel Ann Filosopo <mariel.filosopo@webriq.services>
2022-10-26 18:40:07 +0800

    temp fix for indefinite reload on preview mode with ECWID
    Ref commit: https://github.com/webriq-pagebuilder/site-template-default/commit/de39ea33330169f5e91f7af46f70c780dabd1224

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,26 @@ import "swiper/scss";
 import "swiper/scss/navigation";
 import "swiper/scss/pagination";
 import useScript from "utils/useScript";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
   let script_status = useScript(process.env.NEXT_PUBLIC_ECWID_SCRIPT);
+  const { preview } = pageProps;
+  const router = useRouter();
+
+  // patch: cleanup `secret=<secret>&slug=<slug>` when on preview mode as this causes ECWID to refresh indefinitely
+  useEffect(() => {
+    if (
+      typeof window !== "undefined" &&
+      preview &&
+      location.search?.includes("secret=") &&
+      location.search?.includes("slug=")
+    ) {
+      router.push(`${window.location.pathname}`);
+    }
+  }, [preview, router]);
+
+  // for Ecwid script
   useEffect(() => {
     if (script_status === "ready") {
       try {
