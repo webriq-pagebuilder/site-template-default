7050c7337c825b1e4aae5646db59f11b2ef66c1b
Mariel Ann Filosopo <mariel.filosopo@webriq.services>
2022-10-31 17:57:24 +0800

    Code updates for the nav variant_e search option

--- component/sections/navigation/variant_e.js
+++ component/sections/navigation/variant_e.js
@@ -84,6 +84,19 @@ function VariantE({ banner, logo, links }) {
     }
   }
 
+  useEffect(() => {
+    //assign the ref's current value to the productQuery hook
+    prevQuery.current = productQuery;
+  }, [productQuery]); //run this code when the value of productQuery changes
+
+  // Add query param to /search page based on search input
+  const handleSearchRouting = (e) => {
+    const q = document.getElementById("query");
+    e.preventDefault();
+    setProductQuery(q.value);
+    router.push(`/search?q=${productQuery}`, undefined, { shallow: true });
+  };
+
   return (
     <EcwidContextProvider>
       <section className="relative">
@@ -208,6 +221,7 @@ function VariantE({ banner, logo, links }) {
           </div>
           {/* larger screens search, cart and account icons/buttons */}
           <div className="hidden xl:flex items-center justify-end mr-12">
+            {/* Search button */}
             <button aria-label="search button" type="button">
               <svg
                 width={24}
@@ -219,7 +233,49 @@ function VariantE({ banner, logo, links }) {
                 <path d="M15.853 16.56c-1.683 1.517-3.911 2.44-6.353 2.44-5.243 0-9.5-4.257-9.5-9.5s4.257-9.5 9.5-9.5 9.5 4.257 9.5 9.5c0 2.442-.923 4.67-2.44 6.353l7.44 7.44-.707.707-7.44-7.44zm-6.353-15.56c4.691 0 8.5 3.809 8.5 8.5s-3.809 8.5-8.5 8.5-8.5-3.809-8.5-8.5 3.809-8.5 8.5-8.5z" />
               </svg>
             </button>
-
+            {showSearchBar && (
+              <form
+                id="form"
+                className="flex mr-auto mb-10 lg:mb-0 items-center pl-8 bg-white rounded-lg"
+                method="get"
+                role="search"
+                onSubmit={handleSearchRouting}
+              >
+                <input
+                  id="query"
+                  name="query"
+                  aria-label="Search product"
+                  className="mt-1 mr-3 block w-full p-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-webriq-blue focus:ring-1 focus:ring-webriq-blue"
+                  placeholder="Search..."
+                  onChange={(e) => setProductQuery(e.target.value)}
+                  type="search"
+                />
+                <button
+                  aria-label="Submit product search"
+                  className={`inline-flex items-center justify-center w-12 h-8 bg-webriq-darkblue rounded-md ${
+                    productQuery === ""
+                      ? "cursor-not-allowed opacity-50"
+                      : "hover:bg-webriq-blue transition duration-200"
+                  }`}
+                  disabled={productQuery === ""}
+                  type="submit"
+                >
+                  <svg
+                    width={7}
+                    height={12}
+                    viewBox="0 0 7 12"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M4.125 6.00252L0 1.87752L1.17801 0.699219L6.48102 6.00252L1.17801 11.3058L0 10.1275L4.125 6.00252Z"
+                      fill="white"
+                    />
+                  </svg>
+                </button>
+              </form>
+            )}
+            {/* Cart */}
             <div className="mx-10 cart-icon">
               <div data-icon="BAG" className="ec-cart-widget" />
               <a
@@ -228,7 +284,7 @@ function VariantE({ banner, logo, links }) {
                 className="cart-link"
               />
             </div>
-
+            {/* Account */}
             <a href="/cart?store-page=account">
               <svg
                 width={32}
