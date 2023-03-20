f9a09e6a752b02f1ea59f81be32a8740d753d83f
Mariel Ann Filosopo <mariel.filosopo@webriq.services>
2023-03-20 15:09:03 +0800

    initial commit

---
+++ studio/components/previews/seo/GoogleSearchResults.tsx
@@ -0,0 +1,30 @@
+/* eslint-disable react/no-multi-comp, react/no-did-mount-set-state, react/forbid-prop-types, react/prop-types */
+import React from "react";
+import { assemblePageUrl } from "./frontendUtils";
+import { format } from "date-fns"
+
+function GoogleSearchResult (props) {
+  const { document, options, width = 500 } = props;
+  const { title, seo } = document;
+  const url = assemblePageUrl({ document, options });
+  const date = format(new Date(document?._updatedAt), "MMM dd, yyyy")
+
+  return (
+    <div className="seoItem">
+      <h3>Google search result preview</h3>
+      <div className="googleWrapper" style={{ width }}>
+        <div className="url">{url}</div>
+        <div className="title">
+          {title}
+        </div>
+        {seo?.seoDescription && (
+          <div className="description">
+            {`${date} — ${seo?.seoDescription}`}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default GoogleSearchResult;
---
+++ studio/resolvePreviewUrl.js
@@ -0,0 +1,62 @@
+import {
+  SITE_PREVIEW_SECRET,
+  NEXT_PUBLIC_SITE_URL,
+  NEXT_PUBLIC_NETLIFY_SITE_URL,
+} from "./config"
+//import type { SanityDocumentLike } from "sanity"
+
+export default function resolveProductionUrl(doc) {
+  const currentSlug = doc?.slug?.current || ""
+  const previewUrl = `api/preview?secret=${SITE_PREVIEW_SECRET}&slug=${currentSlug}`
+
+  // only show the "Open Preview" option for the following documents
+  if (["page", "post"].includes(doc?._type)) {
+    if (window.location.hostname.includes("localhost")) {
+      return `${NEXT_PUBLIC_SITE_URL || "http://localhost:3000"}/${previewUrl}`
+    }
+
+    return `${NEXT_PUBLIC_NETLIFY_SITE_URL}/${previewUrl}`
+  } else if (doc?._type === "mainProduct") {
+    if (window.location.hostname.includes("localhost")) {
+      return `${
+        NEXT_PUBLIC_SITE_URL || "http://localhost:3000"
+      }/api/preview?secret=${SITE_PREVIEW_SECRET}&type=products&slug=${currentSlug}`
+    }
+
+    return `${NEXT_PUBLIC_NETLIFY_SITE_URL}/api/preview?secret=${SITE_PREVIEW_SECRET}&type=products&slug=${currentSlug}`
+  } else if (doc?._type === "mainCollection") {
+    if (window.location.hostname.includes("localhost")) {
+      return `${
+        NEXT_PUBLIC_SITE_URL || "http://localhost:3000"
+      }/api/preview?secret=${SITE_PREVIEW_SECRET}&type=collections&slug=${currentSlug}`
+    }
+
+    return `${NEXT_PUBLIC_NETLIFY_SITE_URL}/api/preview?secret=${SITE_PREVIEW_SECRET}&type=collections&slug=${currentSlug}`
+  } else if (doc?._type === "cartPage") {
+    if (window.location.hostname.includes("localhost")) {
+      return `${
+        NEXT_PUBLIC_SITE_URL || "http://localhost:3000"
+      }/api/preview?secret=${SITE_PREVIEW_SECRET}&slug=cart`
+    }
+
+    return `${NEXT_PUBLIC_NETLIFY_SITE_URL}/api/preview?secret=${SITE_PREVIEW_SECRET}&slug=cart`
+  } else if (doc?._type === "wishlistPage") {
+    if (window.location.hostname.includes("localhost")) {
+      return `${
+        NEXT_PUBLIC_SITE_URL || "http://localhost:3000"
+      }/api/preview?secret=${SITE_PREVIEW_SECRET}&slug=wishlist`
+    }
+
+    return `${NEXT_PUBLIC_NETLIFY_SITE_URL}/api/preview?secret=${SITE_PREVIEW_SECRET}&slug=wishlist`
+  } else if (doc?._type === "searchPage") {
+    if (window.location.hostname.includes("localhost")) {
+      return `${
+        NEXT_PUBLIC_SITE_URL || "http://localhost:3000"
+      }/api/preview?secret=${SITE_PREVIEW_SECRET}&slug=search`
+    }
+
+    return `${NEXT_PUBLIC_NETLIFY_SITE_URL}/api/preview?secret=${SITE_PREVIEW_SECRET}&slug=search`
+  }
+
+  return undefined
+}
