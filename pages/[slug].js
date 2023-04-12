03b26cdbf514982157a5193120a46976ba66b08c
Mariel Ann Filosopo <mariel.filosopo@webriq.services>
2023-04-12 14:27:50 +0800

    Initial commit resolve large bundles onPostBuild event
    Reference: https://docs.netlify.com/integrations/frameworks/next-js/troubleshooting/#large-function-error

--- pages/[slug].js
+++ pages/[slug].js
@@ -159,7 +159,7 @@ export async function getStaticPaths() {
 
   return {
     paths: paths.map((slug) => ({ params: { slug } })),
-    fallback: true,
+    fallback: "blocking", // rendering will be deferred until the first user requests the page
   };
 }
 
