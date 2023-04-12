dad28bbeae39c03bd09953e4b85ab69c38f9e0a4
Mariel Ann Filosopo <mariel.filosopo@webriq.services>
2023-04-12 11:12:26 +0800

    Fix scrolling and initial pane sizes when editing the section

--- components/EditSection.js
+++ components/EditSection.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useMagicRouter } from 'hooks'
 import { StudioLayout, StudioProvider } from "sanity"
-import SplitPane, { Pane } from "split-pane-react";
+import SplitPane, { Pane, SashContent } from "split-pane-react";
 import "split-pane-react/esm/themes/default.css"
 import config from "sanity.config"
 
@@ -10,11 +10,7 @@ function EditSection({ documentType, documentId, children }) {
   const history = useMagicRouter(`/studio/desk/__edit__${documentId},type=${documentType}`)
   const [splitPane, setSplitPane] = React.useState(false)
 
-  const [sizes, setSizes] = React.useState([
-    100,
-    '30%',
-    'auto',
-  ]);
+  const [sizes, setSizes] = React.useState([350, 250]);
 
   return (
     <>
@@ -28,26 +24,29 @@ function EditSection({ documentType, documentId, children }) {
       {splitPane ? (
         <div style={{ height: 500 }}>
           <SplitPane
-            split='vertical'
             sizes={sizes}
             onChange={setSizes}
-            className=" border-y border-webriq-darkblue"
+            resizerSize={10}
+            className="border-y border-webriq-darkblue"
+            sashRender={() => (
+              <SashContent style={{ backgroundColor: "#d5e3ff" }} />
+            )}
           >
-            <div className="bg-gray-50">
-              {children}
-            </div>
-            <Pane minSize={50} maxSize='50%'>
-              <div className="overflow-y-scroll">
-                <StudioProvider
-                  config={config}
-                  unstable_history={history}
-                  unstable_noAuthBoundary
-                >
-                  <div className="inline-nav-panesearch inline-nav-paneheader inline-desk-listpane inline-footer">
-                    <StudioLayout />
-                  </div>
-                </StudioProvider>     
+            <Pane style={{ overflowY: "scroll" }}>
+              <div className="bg-gray-50 overflow-y-scroll">
+                {children}
               </div>
+            </Pane>
+            <Pane minSize={50} maxSize='50%' style={{ overflowY: "scroll" }}>
+              <StudioProvider
+                config={config}
+                unstable_history={history}
+                unstable_noAuthBoundary
+              >
+                <div className="inline-nav-panesearch inline-nav-paneheader inline-desk-listpane inline-footer">
+                  <StudioLayout />
+                </div>
+              </StudioProvider>    
             </Pane>
           </SplitPane>
         </div>
