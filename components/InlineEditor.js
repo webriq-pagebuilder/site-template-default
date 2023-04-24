eb7c96521e65993ba61346354a663b08c0c3390d
Mariel Ann Filosopo <mariel.filosopo@webriq.services>
2023-04-24 15:29:23 +0800

    Update inline editor close button

--- components/InlineEditor.js
+++ components/InlineEditor.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useMagicRouter } from "hooks";
-import { StudioLayout, StudioProvider } from "sanity";
+import { StudioLayout, StudioProvider, IntentButton } from "sanity";
 import SplitPane, { Pane, SashContent } from "split-pane-react";
 import "split-pane-react/esm/themes/default.css"
 import config from "sanity.config";
@@ -44,21 +44,22 @@ export default function InlineEditor({ document, showInlineEditor, children }) {
   } 
 
   return (
-    <div className={`show-button ${(!splitPane && !whitelistedTypes?.includes(document?.type)) && "inline-editor"}`}>
+    <div className={`${(!splitPane && !whitelistedTypes?.includes(document?.type)) && "inline-editor show-button"}`}>
       {(windowSize?.width >= 1024 && !whitelistedTypes?.includes(document?.type)) && (
         <button 
           id={document?.type} 
-          className={`hide font-medium shadow-lg px-2 py-2.5 text-sm text-center items-center mt-2 ${
-            splitPane ? "bg-webriq-darkblue text-white hover:bg-webriq-blue hover:text-white" 
-                : "bg-white text-webriq-darkblue border border-webriq-darkblue hover:bg-webriq-blue hover:text-white hover:border-webriq-blue"}`}
+          className={`font-medium shadow-lg px-2 py-2.5 text-sm text-center items-center mt-2 bg-white text-webriq-darkblue border border-webriq-darkblue hover:border-webriq-blue hover:bg-webriq-blue hover:text-white ${splitPane ? "absolute z-40 right-2" : "hide"}`}
           onClick={() => setSplitPane(!splitPane)}
         >
-          {!splitPane && (
+          {!splitPane ? (
             <svg className="fill-current w-4 h-4" clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit={2} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path d="m9.134 19.319 11.587-11.588c.171-.171.279-.423.279-.684 0-.229-.083-.466-.28-.662l-3.115-3.104c-.185-.185-.429-.277-.672-.277s-.486.092-.672.277l-11.606 11.566c-.569 1.763-1.555 4.823-1.626 5.081-.02.075-.029.15-.029.224 0 .461.349.848.765.848.511 0 .991-.189 5.369-1.681zm-3.27-3.342 2.137 2.137-3.168 1.046zm.955-1.166 10.114-10.079 2.335 2.327-10.099 10.101z" fillRule="nonzero" />
             </svg>
+          ): (
+            <svg className="fill-current w-4 h-4" clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit={2} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path d="m12 10.93 5.719-5.72c.146-.146.339-.219.531-.219.404 0 .75.324.75.749 0 .193-.073.385-.219.532l-5.72 5.719 5.719 5.719c.147.147.22.339.22.531 0 .427-.349.75-.75.75-.192 0-.385-.073-.531-.219l-5.719-5.719-5.719 5.719c-.146.146-.339.219-.531.219-.401 0-.75-.323-.75-.75 0-.192.073-.384.22-.531l5.719-5.719-5.72-5.719c-.146-.147-.219-.339-.219-.532 0-.425.346-.749.75-.749.192 0 .385.073.531.219z" />
+            </svg>
           )}
-          <span>{splitPane && "Close"}</span>
         </button>
       )}
       {/* TODO: [Improvement] Add feature to view pane in different device screen sizes */}
