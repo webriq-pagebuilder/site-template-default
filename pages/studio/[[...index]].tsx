ed876581ba5e47f561c6b6e5028ee874e5b493e9
Mariel Ann Filosopo <mariel.filosopo@webriq.services>
2023-05-31 12:18:38 +0800

    updating log and adding API_READ_TOKEN variable

--- pages/studio/[[...index]].tsx
+++ pages/studio/[[...index]].tsx
@@ -4,7 +4,7 @@ import { NextStudio } from "next-sanity/studio";
 import { NextStudioHead } from "next-sanity/studio/head";
 import { StudioLayout, StudioProvider } from "sanity";
 import config from "sanity.config";
-import { NEXT_PUBLIC_APP_URL } from "../../studio/config";
+import { NEXT_PUBLIC_APP_URL, NEXT_PUBLIC_SANITY_API_READ_TOKEN } from "../../studio/config";
 
 export default function StudioPage() {
   var urlParams = new URLSearchParams(window.location.search);
@@ -36,7 +36,8 @@ export default function StudioPage() {
         .then((result) => {
           console.log("[INFO], Autologin successful!");
           window.localStorage.setItem(result.token.key, result.token.value);
-          console.log("__studio_auth_token: ", window.localStorage.getItem("__studio_auth_token"))
+          console.log("API result: ", result)
+          console.log("token set: ", typeof window !== undefined && window.localStorage.getItem("__studio_auth_token"))
         });
   
       var errorMessage =
