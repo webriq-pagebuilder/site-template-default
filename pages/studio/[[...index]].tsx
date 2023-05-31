74db287adeefa681916c661a5f213c6e2be67db5
Mariel Ann Filosopo <mariel.filosopo@webriq.services>
2023-05-31 13:05:11 +0800

    Update [[...index]].tsx

--- pages/studio/[[...index]].tsx
+++ pages/studio/[[...index]].tsx
@@ -4,66 +4,43 @@ import { NextStudio } from "next-sanity/studio";
 import { NextStudioHead } from "next-sanity/studio/head";
 import { StudioLayout, StudioProvider } from "sanity";
 import config from "sanity.config";
-import { NEXT_PUBLIC_APP_URL, NEXT_PUBLIC_SANITY_API_READ_TOKEN } from "../../studio/config";
 
 export default function StudioPage() {
-  var urlParams = new URLSearchParams(window.location.search);
+  var urlParams = typeof window !== "undefined" && new URLSearchParams(window.location.search);
 
   useEffect(() => {
     console.log("urlParams: ", urlParams);
-    if (urlParams.get("token")) {
-      function cleanUp() {
-        var localStorageItems = { ...window.localStorage };
-        Object.entries(localStorageItems).every((item) => {
-          var [key, value] = item;
-          if (typeof window !== "undefined" && key.startsWith("__studio_auth_token")) {
-            window.localStorage.removeItem(key);
-          }
-        });
-      }
-      
-      cleanUp();
-      
-      if(typeof window !== "undefined") {
-        window.localStorage.setItem("__studio_auth_token", urlParams.get("token"));
-        console.log("[INFO], Successfully set autologin token!");
-      }
-
-      var errorMessage =
-        "Oops, unable to autologin! Please retry or if the problem persists, notify WebriQ about this issue ...";
-      var retries = 0;
-      var confirmYes = confirm("Confirm you want to autologin?");
-      if (confirmYes) {
-        var verifyAutologinId;
-
-        function verifyAutologin() {
-          if (
-            typeof window !== "undefined" &&
-            Object.keys({ ...window.localStorage }).find((item) =>
-              item.startsWith("__studio_auth_token")
-            )
-          ) {
-            clearInterval(verifyAutologinId);
-            window.location.href = "/studio";
-
-            console.log("[INFO], Autologin successful!");
-          }
-  
-          retries++;
-  
-          if (retries >= 20) {
-            alert(errorMessage);
-            cleanUp();
-            window.close();
-          }
+    
+    if (urlParams) {
+      console.log(
+        "Skipping invoking duplicate autologin script as one is already in progress..."
+      );
+    } else {
+      if (urlParams.get("token") && typeof window !== "undefined") {
+        function cleanUp() {
+          var localStorageItems = { ...window.localStorage };
+          Object.entries(localStorageItems).every((item) => {
+            var [key, value] = item;
+            if (key.startsWith("__studio_auth_token")) {
+              window.localStorage.removeItem(key);
+            }
+          });
         }
-  
-        verifyAutologinId = setInterval(verifyAutologin, 500);
-      } else {
+        
         cleanUp();
-        window.close();
-      }
-    } // else block is not run to set the autologin script
+        
+        window.localStorage.setItem("__studio_auth_token", urlParams.get("token"));
+        console.log("[INFO], Autologin successful!");
+  
+        var errorMessage =
+          "Oops, unable to autologin! Please retry or if the problem persists, notify WebriQ about this issue ...";
+        var retries = 0;
+        var confirmYes = confirm("Confirm you want to autologin?");
+        if (confirmYes) {
+          window.location.href = "/studio";
+        } 
+      } // else block is not run to set the autologin script
+    }
   }, [urlParams])
 
   return (
