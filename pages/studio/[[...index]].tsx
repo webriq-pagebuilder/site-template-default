119102f4b85c35b2220d4439207e5d7296c4fe81
Mariel Ann Filosopo <mariel.filosopo@webriq.services>
2023-06-01 21:46:00 +0800

    Fixes

--- pages/studio/[[...index]].tsx
+++ pages/studio/[[...index]].tsx
@@ -1,27 +1,25 @@
 import { useEffect, useState } from "react";
 import Head from "next/head";
+import Image from "next/image";
 import { NextStudio } from "next-sanity/studio";
 import { useRouter } from "next/router";
 import { StudioLayout, StudioProvider } from "sanity";
 import config from "sanity.config";
+import { Spinner, Text } from "@sanity/ui";
 import { NEXT_PUBLIC_APP_URL } from "studio/config";
 import AutologinPrepage from "studio/components/AutologinPrepage";
 
 export default function StudioPage() {
   const router = useRouter();
-  const startTime = Date.now();
-  let intervalCount = 0; // interval count
+  var verifyAutologinId, retries = 0;
 
   const [isReady, setIsReady] = useState(true);
   const [isAutologin, setIsAutologin] = useState(false);
+  const [retryAutologin, setRetryAutologin] = useState(0);
 
   useEffect(() => {
-    if(router?.query?.token !== undefined && typeof window !== "undefined") {  
-      const urlParams = new URLSearchParams(window.location.search); 
-
-      setIsAutologin(true);
-      setIsReady(false);
-
+    const urlParams = new URLSearchParams(window.location.search);
+    if(router.query.token !== undefined && typeof window !== "undefined") {  
       // cleanup (localStorage)
       function cleanUp() {
         const localStorageItems = { ...window.localStorage };
@@ -35,49 +33,48 @@ export default function StudioPage() {
       cleanUp();
 
       // set token
-      async function fetchAutologinToken() {
-        // 15-second duration with 3 retries (5s per retry)
-        while(!isReady && Date.now() - startTime < 3 * 5 * 1000) {
-          // increment per interval then log current count
-          intervalCount++;
+      function fetchAutologinToken() {
+        try {
+          console.log("Start setting up login to studio...");
 
-          console.log("🚀 ~ auto login attempts: ", intervalCount);
+          setIsAutologin(true);
+          setIsReady(false);
 
-          try {
-            console.log("Start setting up login to studio...");
-            await fetch(`${NEXT_PUBLIC_APP_URL}/api/studio?${urlParams}`)
-              .then((res) => {
-                if (!res.ok) {
-                  cleanUp();
-                  setIsReady(false);
-                  console.log("[INFO] Unable to fetch autologin token! ", res);
-                }
+          fetch(`${NEXT_PUBLIC_APP_URL}/api/studio?${urlParams}`)
+            .then((res) => {
+              if (!res.ok) {
+                cleanUp();
+                console.log("[INFO] Unable to fetch autologin token! ");
+              }
 
-                return res.json();
-              })
-              .then((result) => {
-                window.localStorage.setItem([result.token.key], result.token.value);
+              return res.json();
+            })
+            .then((result) => {
+              window.localStorage.setItem([result?.token?.key], result?.token?.value);
 
-                // verify if value was added to localStorage
-                if(!window.localStorage.getItem(result.token.key)) {
-                  console.log("Failed to set autologin token! Retrying...")
-                } else {
-                  console.log("[INFO] Successfully set autologin token!");
+              // verify if value was added to localStorage
+              if(window.localStorage.getItem(result?.token?.key) !== null) {
+                clearInterval(verifyAutologinId);
+                console.log("[INFO] Successfully set autologin token!");
+                setIsReady(true);
+              } 
 
-                  setIsReady(true);
-                  setIsAutologin(true);
-                  router.reload();
-                }
-              })
-          } catch(error) {
-            console.log("[ERROR] Something went wrong. Failed to process autologin request. ", error);
-            setIsReady(false);
-          }
+              retries++;
+      
+              if (retries >= 20) {
+                clearInterval(verifyAutologinId);
+                setRetryAutologin(retries);
+              }
+            })
+          console.log("Autologin status: ", { ready: isReady, autologin: isAutologin, retries: retries });
+        } catch(error) {
+          console.log("[ERROR] Something went wrong. Failed to process autologin request. ", error);
         }
-      }
-      fetchAutologinToken();
+      };
+
+      verifyAutologinId = setInterval(fetchAutologinToken, 500);
     } 
-  }, [router])
+  }, [router.query])
 
   return (
     <>
@@ -88,11 +85,38 @@ export default function StudioPage() {
       <NextStudio config={config}>
         <StudioProvider config={config}>
           {!isReady && isAutologin ? (
-            <AutologinPrepage 
-              ready={isReady}
-              retrying={Date.now() - startTime < 3 * 5 * 1000}
-            />
-          ): <StudioLayout />}
+            retryAutologin >= 20 ? (
+              <AutologinPrepage>
+                <Image 
+                  src="/assets/elements/Settings_Monochromatic-01.svg"
+                  width={500}
+                  height={500}
+                  alt="Settings_Monochromatic-01"
+                />
+                <Text size={3} weight="bold">
+                  Oops, unable to autologin!
+                </Text>
+                <Text muted size={1}>
+                  Please notify WebriQ about this issue...
+                </Text>
+              </AutologinPrepage>
+            ) : (
+              // not ready, autologin, retries is less than or equal 10
+              <AutologinPrepage>
+                {/* <Spinner /> */}
+                <span className="inline-block w-14 h-14 mb-5 border-4 border-webriq-babyblue border-b-slate-300 rounded-full animate-spin"/>
+                <Text size={3} weight="bold">
+                  Logging in to WebriQ Studio
+                </Text>
+                <Text className="animate-pulse" muted size={1}>
+                  Please wait
+                </Text>
+              </AutologinPrepage>
+            )
+          ) : (
+            // ready, not autologin
+            <StudioLayout />
+          )}
         </StudioProvider>
       </NextStudio>
     </>
--- studio/components/AutologinPrepage.tsx
+++ studio/components/AutologinPrepage.tsx
@@ -1,6 +1,6 @@
-import { Card, Flex, Spinner, Text } from "@sanity/ui";
+import { Card, Flex } from "@sanity/ui";
 
-export default function AutologinPrepage({ready, retrying}: {ready: boolean, retrying: boolean}) {
+export default function AutologinPrepage({ children }) {  
   return (
     <Card padding={8}>
       <Flex
@@ -9,28 +9,8 @@ export default function AutologinPrepage({ready, retrying}: {ready: boolean, ret
         gap={5}
         height="fill"
         justify="center"
-        >
-        {!ready && !retrying ? (
-          <>
-            <Text size={3} weight="bold">
-              Oops, unable to autologin!
-            </Text>
-            <Text muted size={1}>
-              Please notify WebriQ about this issue...
-            </Text>
-          </>
-        ) : (
-          // auto login is still in progress
-          <>
-            <Spinner size={4} />
-            <Text size={3} weight="bold">
-              Logging in to WebriQ Studio
-            </Text>
-            <Text muted size={1}>
-              Please wait...
-            </Text>
-          </>
-        )}
+      >
+        {children}
       </Flex>
     </Card>
   );
