9ff963de7a8494a0bc86c08e6bbf618a800b29f5
Dorell WebriQ <info@webriq.com>
2023-09-27 21:19:01 +0800

    feat: only really load the ecwid script when there are sections that need it

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,48 +10,50 @@ import { filterDataToSingleItem, SEO } from "components/list";
 import { PreviewBanner } from "components/PreviewBanner";
 import InlineEditorContextProvider from "context/InlineEditorContext";
 import { CommonPageData, DefaultSeoData } from "types";
+import { hasCStudioComponents } from "helper/hasCStudioComponents";
 
 interface HomeProps {
-	data: Data;
-	preview: boolean;
-	token?: string | null;
-	source?: string;
-	defaultSeo: DefaultSeoData;
+  data: Data;
+  preview: boolean;
+  token?: string | null;
+  source?: string;
+  defaultSeo: DefaultSeoData;
+  hasCStudioComponents?: boolean;
 }
 
 interface DocumentWithPreviewProps {
-	data: Data;
-	token: string | null;
-	defaultSeo: DefaultSeoData;
+  data: Data;
+  token: string | null;
+  defaultSeo: DefaultSeoData;
 }
 
 interface Data {
-	pageData: PageData | null;
+  pageData: PageData | null;
 }
 
 interface PageData extends CommonPageData {
-	collections: any;
-	slug: string | string[];
-	title: string;
+  collections: any;
+  slug: string | string[];
+  title: string;
 }
 
 function Home({ data, preview, token, source, defaultSeo }: HomeProps) {
-	const showInlineEditor = source === "studio";
+  const showInlineEditor = source === "studio";
 
-	if (preview) {
-		return (
-			<>
-				<PreviewBanner />
-				<PreviewSuspense fallback="Loading...">
-					<InlineEditorContextProvider showInlineEditor={showInlineEditor}>
-						<DocumentWithPreview {...{ data, token, defaultSeo }} />
-					</InlineEditorContextProvider>
-				</PreviewSuspense>
-			</>
-		);
-	}
+  if (preview) {
+    return (
+      <>
+        <PreviewBanner />
+        <PreviewSuspense fallback="Loading...">
+          <InlineEditorContextProvider showInlineEditor={showInlineEditor}>
+            <DocumentWithPreview {...{ data, token, defaultSeo }} />
+          </InlineEditorContextProvider>
+        </PreviewSuspense>
+      </>
+    );
+  }
 
-	return <Document {...{ data, defaultSeo }} />;
+  return <Document {...{ data, defaultSeo }} />;
 }
 
 /**
@@ -61,40 +63,40 @@ function Home({ data, preview, token, source, defaultSeo }: HomeProps) {
  * @returns Document with published data
  */
 function Document({
-	data,
-	defaultSeo,
+  data,
+  defaultSeo,
 }: {
-	data: Data;
-	defaultSeo: DefaultSeoData;
+  data: Data;
+  defaultSeo: DefaultSeoData;
 }) {
-	const publishedData = data?.pageData;
+  const publishedData = data?.pageData;
 
-	// General safeguard against empty data
-	if (!publishedData) {
-		return null;
-	}
+  // General safeguard against empty data
+  if (!publishedData) {
+    return null;
+  }
 
-	const { title, _type, seo } = publishedData;
+  const { title, _type, seo } = publishedData;
 
-	return (
-		<>
-			<Head>
-				<SEO
-					data={{
-						pageTitle: title,
-						type: _type,
-						route: publishedData?.slug,
-						...seo,
-					}}
-					defaultSeo={defaultSeo}
-				/>
-				<title>{seo?.seoTitle ?? title ?? "WebriQ Studio"}</title>
-			</Head>
+  return (
+    <>
+      <Head>
+        <SEO
+          data={{
+            pageTitle: title,
+            type: _type,
+            route: publishedData?.slug,
+            ...seo,
+          }}
+          defaultSeo={defaultSeo}
+        />
+        <title>{seo?.seoTitle ?? title ?? "WebriQ Studio"}</title>
+      </Head>
 
-			{/*  Show page sections */}
-			{data?.pageData && <PageSections data={publishedData} />}
-		</>
-	);
+      {/*  Show page sections */}
+      {data?.pageData && <PageSections data={publishedData} />}
+    </>
+  );
 }
 
 /**
@@ -106,86 +108,87 @@ function Document({
  * @returns Document with preview data
  */
 function DocumentWithPreview({
-	data,
-	token = null,
-	defaultSeo,
+  data,
+  token = null,
+  defaultSeo,
 }: DocumentWithPreviewProps) {
-	const previewDataEventSource = usePreview(token, homeQuery);
+  const previewDataEventSource = usePreview(token, homeQuery);
 
-	const previewData: PageData =
-		previewDataEventSource?.[0] || previewDataEventSource;
+  const previewData: PageData =
+    previewDataEventSource?.[0] || previewDataEventSource;
 
-	// General safeguard against empty data
-	if (!previewData) {
-		return null;
-	}
+  // General safeguard against empty data
+  if (!previewData) {
+    return null;
+  }
 
-	const { title, _type, seo } = previewData;
+  const { title, _type, seo } = previewData;
 
-	return (
-		<>
-			<Head>
-				<SEO
-					data={{
-						pageTitle: title,
-						type: _type,
-						route: previewData?.slug,
-						...seo,
-					}}
-					defaultSeo={defaultSeo}
-				/>
-				<title>{seo?.seoTitle ?? title ?? "WebriQ Studio"}</title>
-			</Head>
+  return (
+    <>
+      <Head>
+        <SEO
+          data={{
+            pageTitle: title,
+            type: _type,
+            route: previewData?.slug,
+            ...seo,
+          }}
+          defaultSeo={defaultSeo}
+        />
+        <title>{seo?.seoTitle ?? title ?? "WebriQ Studio"}</title>
+      </Head>
 
-			{/* if no sections, show no sections only in preview */}
+      {/* if no sections, show no sections only in preview */}
 
-			{(!previewData ||
-				!previewData?.sections ||
-				previewData?.sections?.length === 0) && <PreviewNoContent />}
+      {(!previewData ||
+        !previewData?.sections ||
+        previewData?.sections?.length === 0) && <PreviewNoContent />}
 
-			{/*  Show page sections */}
-			{data?.pageData && <PageSections data={previewData} />}
-		</>
-	);
+      {/*  Show page sections */}
+      {data?.pageData && <PageSections data={previewData} />}
+    </>
+  );
 }
 
 export const getStaticProps = async ({
-	preview = false,
-	previewData = {},
+  preview = false,
+  previewData = {},
 }: any): Promise<{ props: HomeProps }> => {
-	const client =
-		preview && previewData?.token
-			? getClient(false).withConfig({ token: previewData.token })
-			: getClient(preview);
+  const client =
+    preview && previewData?.token
+      ? getClient(false).withConfig({ token: previewData.token })
+      : getClient(preview);
 
-	const [indexPage, globalSEO] = await Promise.all([
-		client.fetch(homeQuery),
-		client.fetch(globalSEOQuery),
-	]);
+  const [indexPage, globalSEO] = await Promise.all([
+    client.fetch(homeQuery),
+    client.fetch(globalSEOQuery),
+  ]);
 
-	// pass page data and preview to helper function
-	const pageData: PageData = filterDataToSingleItem(indexPage, preview);
+  // pass page data and preview to helper function
+  const pageData: PageData = filterDataToSingleItem(indexPage, preview);
 
-	// if our query failed, then return null to display custom no-preview page
-	if (!pageData) {
-		return {
-			props: {
-				preview,
-				data: { pageData: null },
-				defaultSeo: globalSEO,
-			},
-		};
-	}
+  // if our query failed, then return null to display custom no-preview page
+  if (!pageData) {
+    return {
+      props: {
+        preview,
+        data: { pageData: null },
+        defaultSeo: globalSEO,
+      },
+    };
+  }
 
-	return {
-		props: {
-			preview,
-			token: (preview && previewData.token) || "",
-			source: (preview && previewData.source) || "",
-			data: { pageData },
-			defaultSeo: globalSEO,
-		},
-	};
+  return {
+    props: {
+      preview,
+      token: (preview && previewData.token) || "",
+      source: (preview && previewData.source) || "",
+      data: { pageData },
+      defaultSeo: globalSEO,
+      hasCStudioComponents: hasCStudioComponents(pageData?.sections),
+    },
+  };
 };
 
 export default Home;
