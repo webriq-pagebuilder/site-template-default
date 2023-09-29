8843c8330b2108600c5eeb5210b488f76f379ea9
Dorell WebriQ <info@webriq.com>
2023-09-29 11:45:34 +0800

    apply hasCStudioComponents to other pages

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -12,47 +12,47 @@ import InlineEditorContextProvider from "context/InlineEditorContext";
 import { CommonPageData, DefaultSeoData } from "types";
 
 interface CartPageProps {
-	data: Data;
-	preview: boolean;
-	token?: string;
-	source?: string;
-	defaultSeo: DefaultSeoData;
+  data: Data;
+  preview: boolean;
+  token?: string;
+  source?: string;
+  defaultSeo: DefaultSeoData;
 }
 
 interface Data {
-	cartData: CartData;
+  cartData: CartData;
 }
 export interface CartData extends CommonPageData {
-	cartSectionVariant?: {
-		variant: string;
-		_type: string;
-	};
-	id?: string;
+  cartSectionVariant?: {
+    variant: string;
+    _type: string;
+  };
+  id?: string;
 }
 
 interface DocumentWithPreviewProps {
-	data: Data;
-	token: string;
-	defaultSeo: DefaultSeoData;
+  data: Data;
+  token: string;
+  defaultSeo: DefaultSeoData;
 }
 
 function CartPage({ data, preview, token, source, defaultSeo }: CartPageProps) {
-	const showInlineEditor = source === "studio";
+  const showInlineEditor = source === "studio";
 
-	if (preview) {
-		return (
-			<>
-				<PreviewBanner />
-				<PreviewSuspense fallback="Loading">
-					<InlineEditorContextProvider showInlineEditor={showInlineEditor}>
-						<DocumentWithPreview {...{ data, token, defaultSeo }} />
-					</InlineEditorContextProvider>
-				</PreviewSuspense>
-			</>
-		);
-	}
+  if (preview) {
+    return (
+      <>
+        <PreviewBanner />
+        <PreviewSuspense fallback="Loading">
+          <InlineEditorContextProvider showInlineEditor={showInlineEditor}>
+            <DocumentWithPreview {...{ data, token, defaultSeo }} />
+          </InlineEditorContextProvider>
+        </PreviewSuspense>
+      </>
+    );
+  }
 
-	return <Document {...{ data, defaultSeo }} />;
+  return <Document {...{ data, defaultSeo }} />;
 }
 
 /**
@@ -62,35 +62,35 @@ function CartPage({ data, preview, token, source, defaultSeo }: CartPageProps) {
  * @returns Document with published data
  */
 function Document({
-	data,
-	defaultSeo,
+  data,
+  defaultSeo,
 }: {
-	data: Data;
-	defaultSeo: DefaultSeoData;
+  data: Data;
+  defaultSeo: DefaultSeoData;
 }) {
-	const publishedData = data?.cartData;
+  const publishedData = data?.cartData;
 
-	// General safeguard against empty data
-	if (!publishedData) {
-		return null;
-	}
+  // General safeguard against empty data
+  if (!publishedData) {
+    return null;
+  }
 
-	const { seo, _type } = publishedData;
+  const { seo, _type } = publishedData;
 
-	return (
-		<>
-			<Head>
-				<SEO
-					data={{ pageTitle: "Cart", type: _type, route: "cart", ...seo }}
-					defaultSeo={defaultSeo}
-				/>
-				<title>{seo?.seoTitle ?? "Cart"}</title>
-			</Head>
+  return (
+    <>
+      <Head>
+        <SEO
+          data={{ pageTitle: "Cart", type: _type, route: "cart", ...seo }}
+          defaultSeo={defaultSeo}
+        />
+        <title>{seo?.seoTitle ?? "Cart"}</title>
+      </Head>
 
-			{/*  Show page sections */}
-			{data?.cartData && <CartSections data={publishedData} />}
-		</>
-	);
+      {/*  Show page sections */}
+      {data?.cartData && <CartSections data={publishedData} />}
+    </>
+  );
 }
 
 /**
@@ -101,79 +101,80 @@ function Document({
  * @returns Document with preview data
  */
 function DocumentWithPreview({
-	data,
-	token = null,
-	defaultSeo,
+  data,
+  token = null,
+  defaultSeo,
 }: DocumentWithPreviewProps) {
-	const previewDataEventSource = usePreview(token, cartPageQuery);
-	const previewData: CartData =
-		previewDataEventSource?.[0] || previewDataEventSource;
+  const previewDataEventSource = usePreview(token, cartPageQuery);
+  const previewData: CartData =
+    previewDataEventSource?.[0] || previewDataEventSource;
 
-	// General safeguard against empty data
-	if (!previewData) {
-		return null;
-	}
+  // General safeguard against empty data
+  if (!previewData) {
+    return null;
+  }
 
-	const { seo, _type } = previewData;
+  const { seo, _type } = previewData;
 
-	return (
-		<>
-			<Head>
-				<SEO
-					data={{ pageTitle: "Cart", type: _type, route: "cart", ...seo }}
-					defaultSeo={defaultSeo}
-				/>
-				<title>{seo?.seoTitle ?? "Cart"}</title>
-			</Head>
+  return (
+    <>
+      <Head>
+        <SEO
+          data={{ pageTitle: "Cart", type: _type, route: "cart", ...seo }}
+          defaultSeo={defaultSeo}
+        />
+        <title>{seo?.seoTitle ?? "Cart"}</title>
+      </Head>
 
-			{/* if no sections, show no sections only in preview */}
+      {/* if no sections, show no sections only in preview */}
 
-			{(!previewData ||
-				!previewData?.sections ||
-				previewData?.sections?.length === 0) && <PreviewNoContent />}
+      {(!previewData ||
+        !previewData?.sections ||
+        previewData?.sections?.length === 0) && <PreviewNoContent />}
 
-			{/*  Show page sections */}
-			{data?.cartData && <CartSections data={previewData} />}
-		</>
-	);
+      {/*  Show page sections */}
+      {data?.cartData && <CartSections data={previewData} />}
+    </>
+  );
 }
 
 export async function getStaticProps({
-	preview = false,
-	previewData = {},
+  preview = false,
+  previewData = {},
 }: any): Promise<{ props: CartPageProps }> {
-	const client =
-		preview && previewData?.token
-			? getClient(false).withConfig({ token: previewData.token })
-			: getClient(preview);
+  const client =
+    preview && previewData?.token
+      ? getClient(false).withConfig({ token: previewData.token })
+      : getClient(preview);
 
-	const [cartPage, globalSEO] = await Promise.all([
-		client.fetch(cartPageQuery),
-		client.fetch(globalSEOQuery),
-	]);
+  const [cartPage, globalSEO] = await Promise.all([
+    client.fetch(cartPageQuery),
+    client.fetch(globalSEOQuery),
+  ]);
 
-	// pass page data and preview to helper function
-	const cartData: CartData = filterDataToSingleItem(cartPage, preview);
+  // pass page data and preview to helper function
+  const cartData: CartData = filterDataToSingleItem(cartPage, preview);
 
-	if (!cartData) {
-		return {
-			props: {
-				preview,
-				data: { cartData: null },
-				defaultSeo: globalSEO,
-			},
-		};
-	}
+  if (!cartData) {
+    return {
+      props: {
+        preview,
+        data: { cartData: null },
+        defaultSeo: globalSEO,
+      },
+    };
+  }
 
-	return {
-		props: {
-			preview,
-			token: (preview && previewData.token) || "",
-			source: (preview && previewData.source) || "",
-			data: { cartData },
-			defaultSeo: globalSEO,
-		},
-	};
+  return {
+    props: {
+      preview,
+      token: (preview && previewData.token) || "",
+      source: (preview && previewData.source) || "",
+      data: { cartData },
+      defaultSeo: globalSEO,
+      hasCStudioComponents: true,
+    },
+  };
 }
 
 export default React.memo(CartPage);
