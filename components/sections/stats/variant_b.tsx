63c14987ae89df5682391d0bdf7a01860f461874
Webriq <info@webriq.com>
2024-01-10 10:33:16 +0800

    use stat cards on stats section

--- components/sections/stats/variant_b.tsx
+++ components/sections/stats/variant_b.tsx
@@ -2,34 +2,21 @@ import React from "react";
 import Image from "next/image";
 import { urlFor } from "lib/sanity";
 import { StatsProps } from ".";
+import { StatsCard } from "components/common/stats/stats-card";
 
 function VariantB({ stats }: StatsProps) {
   return (
     <section className="py-20">
-      <div className="container mx-auto px-4">
+      <div className="container px-4 mx-auto">
         {stats && (
-          <div className="sm:justify-left mx-10 items-center sm:-mx-4 sm:flex sm:flex-wrap">
+          <div className="items-center mx-10 sm:justify-left sm:-mx-4 sm:flex sm:flex-wrap">
             {stats?.map((stat, index) => (
-              <div
-                className="my-8 flex w-full items-center px-4 md:w-1/2 lg:w-1/4 lg:justify-center"
-                key={index}
-              >
-                {stat?.mainImage?.image && (
-                  <div className="mr-4 inline-block rounded bg-webriq-lightblue p-4">
-                    <Image
-                      src={urlFor(stat?.mainImage?.image)}
-                      width={24}
-                      height={24}
-                      quality={100}
-                      alt={stat?.mainImage?.alt ?? "statistics-icon"}
-                    />
-                  </div>
-                )}
-                <div>
-                  <p className="text-2xl font-bold">{stat?.value}</p>
-                  <p className="text-gray-500">{stat?.label}</p>
-                </div>
-              </div>
+              <StatsCard
+                className="my-8 md:w-1/2 lg:w-1/4 lg:justify-center"
+                value={stat?.value}
+                label={stat?.label}
+                icon={urlFor(stat?.mainImage?.image)}
+              />
             ))}
           </div>
         )}
