9f69381a9e9d85b47ccd509d804d40557e10166f
Webriq <info@webriq.com>
2024-01-15 15:18:40 +0800

    add theme

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,13 +2,14 @@ import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
   stories: ["../**/*.mdx", "../**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
     "@storybook/addon-docs",
-    "@storybook/addon-mdx-gfm"
+    "@storybook/addon-mdx-gfm",
   ],
   framework: {
     name: "@storybook/nextjs",
