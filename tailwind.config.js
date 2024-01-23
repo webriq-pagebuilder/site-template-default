f19042d4ec21b569278bee762094b42f132c20f7
Webriq <info@webriq.com>
2024-01-23 14:09:16 +0800

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,83 @@
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		"./pages/**/*.{js,ts,jsx,tsx}",
-		"./components/**/*.{js,ts,jsx,tsx}",
-	],
-	theme: {
-		extend: {
-			colors: {
-				"webriq-blue": "#296eff",
-				"webriq-lightblue": "#d5e3ff",
-				"webriq-darkblue": "#0045d8",
-				"webriq-babyblue": "#3576ff",
-			},
-		},
-	},
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "webriq-blue": "#296eff",
+        "webriq-lightblue": "#d5e3ff",
+        "webriq-darkblue": "#0045d8",
+        "webriq-babyblue": "#3576ff",
+        primary: {
+          DEFAULT: "#0045d8",
+          foreground: "#296eff",
+        },
+        secondary: {
+          DEFAULT: "#3576ff",
+          foreground: "#d5e3ff",
+        },
+      },
+    },
+  },
 };
+
+// extend: {
+// 	colors: {
+// 	  border: "hsl(var(--border))",
+// 	  input: "hsl(var(--input))",
+// 	  ring: "hsl(var(--ring))",
+// 	  background: "hsl(var(--background))",
+// 	  foreground: "hsl(var(--foreground))",
+// 	  primary: {
+// 		DEFAULT: "#0045d8",
+// 		foreground: "#296eff",
+// 	  },
+// 	  secondary: {
+// 		DEFAULT: "hsl(var(--secondary))",
+// 		foreground: "hsl(var(--secondary-foreground))",
+// 	  },
+// 	  destructive: {
+// 		DEFAULT: "hsl(var(--destructive))",
+// 		foreground: "hsl(var(--destructive-foreground))",
+// 	  },
+// 	  muted: {
+// 		DEFAULT: "hsl(var(--muted))",
+// 		foreground: "hsl(var(--muted-foreground))",
+// 	  },
+// 	  accent: {
+// 		DEFAULT: "hsl(var(--accent))",
+// 		foreground: "hsl(var(--accent-foreground))",
+// 	  },
+// 	  popover: {
+// 		DEFAULT: "hsl(var(--popover))",
+// 		foreground: "hsl(var(--popover-foreground))",
+// 	  },
+// 	  card: {
+// 		DEFAULT: "hsl(var(--card))",
+// 		foreground: "hsl(var(--card-foreground))",
+// 	  },
+// 	},
+// 	borderRadius: {
+// 	  lg: "var(--radius)",
+// 	  md: "calc(var(--radius) - 2px)",
+// 	  sm: "calc(var(--radius) - 4px)",
+// 	},
+// 	keyframes: {
+// 	  "accordion-down": {
+// 		from: { height: 0 },
+// 		to: { height: "var(--radix-accordion-content-height)" },
+// 	  },
+// 	  "accordion-up": {
+// 		from: { height: "var(--radix-accordion-content-height)" },
+// 		to: { height: 0 },
+// 	  },
+// 	},
+// 	animation: {
+// 	  "accordion-down": "accordion-down 0.2s ease-out",
+// 	  "accordion-up": "accordion-up 0.2s ease-out",
+// 	},
+//   },
