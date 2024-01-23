30022087c1c5be15ddd3417f7d3c59c11b9dc8cb
MarielF WebriQ <info@webriq.com>
2024-01-23 21:16:41 +0800

    Initial implementation of hidden fields

--- components/sections/call_to_action/stories/call_to_action.stories.tsx
+++ components/sections/call_to_action/stories/call_to_action.stories.tsx
@@ -2,99 +2,123 @@ import type { Meta, StoryObj } from "@storybook/react";
 import CallToActionComponent, { CTAProps } from "../index";
 import { Sections, SectionsProps, Variants } from "types";
 
-const args: Variants = {
+const args = {
   logo: {
-    alt: "Logo",
-    image: {
-      _type: "image",
-      asset: {
-        _ref: "image-b3b0a815c21cc9fd95261a2a0dd737e0827915cd-664x833-png",
-        _type: "reference",
+    hidden: ["variant_c", "variant_e"],
+    value: {
+      alt: "Logo",
+      image: {
+        _type: "image",
+        asset: {
+          _ref: "image-b3b0a815c21cc9fd95261a2a0dd737e0827915cd-664x833-png",
+          _type: "reference",
+        },
       },
+      type: "linkInternal",
+      internalLink: null,
+      externalLink: null,
     },
-    type: "linkInternal",
-    internalLink: null,
-    externalLink: null,
   },
-  title: "So much more than a business analytics tool",
-  plainText:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque efficitur nisl sodales egestas lobortis.",
+  title: {
+    hidden: ["variant_e"],
+    value: "So much more than a business analytics tool",
+  },
+  plainText: {
+    hidden: ["variant_e"],
+    value:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque efficitur nisl sodales egestas lobortis.",
+  },
   primaryButton: {
-    linkTarget: "_self",
-    _type: "conditionalLink",
-    linkType: "linkInternal",
-    label: "Get Started",
-    type: "linkInternal",
-    internalLink: null,
-    externalLink: null,
+    hidden: ["variant_b", "variant_c", "variant_e"],
+    value: {
+      linkTarget: "_self",
+      _type: "conditionalLink",
+      linkType: "linkInternal",
+      label: "Get Started",
+      type: "linkInternal",
+      internalLink: null,
+      externalLink: null,
+    },
   },
-  tags: ["No credit card needed", "Easy to use"],
-  formLinks: [
-    {
-      _type: "conditionalLink",
-      linkType: "linkInternal",
-      label: "Police privacy",
-      type: "linkInternal",
-      internalLink: null,
-      externalLink: null,
-      _key: "x7RnKCMTO5-QN_-Bjrd3P",
-      linkTarget: "_self",
-    },
-    {
-      _type: "conditionalLink",
-      linkType: "linkInternal",
-      label: "Terms of Use",
-      type: "linkInternal",
-      internalLink: null,
-      externalLink: null,
-      _key: "oPZzd7iEOiTTQskCQelp5",
-      linkTarget: "_self",
-    },
-  ],
-  form: {
-    fields: [
+  tags: {
+    hidden: ["variant_a", "variant_b", "variant_d", "variant_e"],
+    value: ["No credit card needed", "Easy to use"],
+  },
+  formLinks: {
+    hidden: ["variant_a", "variant_b", "variant_c"],
+    value: [
       {
-        _type: "webriqFormField",
-        name: "First Name",
-        placeholder: "First Name",
-        _key: "dSFFjMBkXVDZYezFxQ6Rn",
-        type: "inputText",
+        _type: "conditionalLink",
+        linkType: "linkInternal",
+        label: "Police privacy",
+        type: "linkInternal",
+        internalLink: null,
+        externalLink: null,
+        _key: "x7RnKCMTO5-QN_-Bjrd3P",
+        linkTarget: "_self",
       },
       {
-        name: "Last Name",
-        placeholder: "Last Name",
-        _key: "6DMOG1JfjbcysZl5xExNM",
-        type: "inputText",
-        _type: "webriqFormField",
-      },
-      {
-        type: "inputEmail",
-        _type: "webriqFormField",
-        name: "Email",
-        placeholder: "Enter your email address",
-        _key: "kJTXeFPZecA6Vgcz1qRLy",
-      },
-      {
-        type: "inputPassword",
-        _type: "webriqFormField",
-        name: "Password",
-        placeholder: "Enter your password",
-        _key: "wdhApcde2w34sfE4yzf8f",
+        _type: "conditionalLink",
+        linkType: "linkInternal",
+        label: "Terms of Use",
+        type: "linkInternal",
+        internalLink: null,
+        externalLink: null,
+        _key: "oPZzd7iEOiTTQskCQelp5",
+        linkTarget: "_self",
       },
     ],
-    buttonLabel: "Sign Up",
-    thankYouPage: null,
-    subtitle: "Sign Up",
-    name: "Create an account",
+  },
+  form: {
+    hidden: ["variant_a"],
+    value: {
+      name: "Create an account",
+      buttonLabel: "Sign Up",
+      thankYouPage: null,
+      subtitle: "Sign Up",
+      fields: [
+        {
+          _type: "webriqFormField",
+          name: "First Name",
+          placeholder: "First Name",
+          _key: "dSFFjMBkXVDZYezFxQ6Rn",
+          type: "inputText",
+        },
+        {
+          name: "Last Name",
+          placeholder: "Last Name",
+          _key: "6DMOG1JfjbcysZl5xExNM",
+          type: "inputText",
+          _type: "webriqFormField",
+        },
+        {
+          type: "inputEmail",
+          _type: "webriqFormField",
+          name: "Email",
+          placeholder: "Enter your email address",
+          _key: "kJTXeFPZecA6Vgcz1qRLy",
+        },
+        {
+          type: "inputPassword",
+          _type: "webriqFormField",
+          name: "Password",
+          placeholder: "Enter your password",
+          _key: "wdhApcde2w34sfE4yzf8f",
+        },
+      ],
+    },
   },
   signInLink: {
-    linkType: "linkInternal",
-    type: "linkInternal",
-    internalLink: null,
-    externalLink: null,
-    label: "Sign In",
-    linkTarget: "_self",
-    _type: "conditionalLink",
+    hidden: ["variant_a", "variant_b", "variant_c"],
+    value: {
+      linkType: "linkInternal",
+      type: "linkInternal",
+      internalLink: null,
+      externalLink: null,
+      label: "Sign In",
+      linkTarget: "_self",
+      _type: "conditionalLink",
+    },
   },
 };
 
@@ -115,37 +139,47 @@ const meta = {
 export default meta;
 type Story = StoryObj<Sections>;
 
+// filter out args that are not listed on hidden array
+const filteredArgs = (variant: string) => {
+  return Object.keys(args).reduce((result, key) => {
+    if (args?.[key]?.hidden && !args?.[key]?.hidden?.includes(variant)) {
+      result[key] = args[key]?.value;
+    }
+    return result;
+  }, {});
+};
+
 export const variant_a: Story = {
   args: {
     variant: "variant_a",
-    ...args,
+    ...filteredArgs("variant_a"),
   },
 };
 
 export const variant_b: Story = {
   args: {
     variant: "variant_b",
-    ...args,
+    ...filteredArgs("variant_b"),
   },
 };
 
 export const variant_c: Story = {
   args: {
     variant: "variant_c",
-    ...args,
+    ...filteredArgs("variant_c"),
   },
 };
 
 export const variant_d: Story = {
   args: {
     variant: "variant_d",
-    ...args,
+    ...filteredArgs("variant_d"),
   },
 };
 
 export const variant_e: Story = {
   args: {
     variant: "variant_e",
-    ...args,
+    ...filteredArgs("variant_e"),
   },
 };
