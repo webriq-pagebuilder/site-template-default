33fc874aff2c87ec68908cbcfdb0aabae05a1b4b
Webriq <info@webriq.com>
2024-01-26 13:35:33 +0800

    update

--- components/ui/ConditionalLink/ConditionalLink.tsx
+++ components/ui/ConditionalLink/ConditionalLink.tsx
@@ -42,7 +42,7 @@ export const ConditionalLink = ({
 
   const commonProps = {
     className: cn(variantClass, className),
-    ariaLabel,
+    "aria-label": ariaLabel,
     target,
   };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -34,25 +34,3 @@ module.exports = {
     },
   },
 };
-
-// 	colors: {
-// 	  border: "hsl(var(--border))",
-// 	  background: "hsl(var(--background))",
-// 	  primary: {
-// 		DEFAULT: "#0045d8",
-// 		foreground: "#296eff",
-// 	  },
-// 	  secondary: {
-// 		DEFAULT: "hsl(var(--secondary))",
-// 		foreground: "hsl(var(--secondary-foreground))",
-// 	  },
-// 	  muted: {
-// 		DEFAULT: "hsl(var(--muted))",
-// 		foreground: "hsl(var(--muted-foreground))",
-// 	  },
-// 	},
-// 	borderRadius: {
-// 	  lg: "var(--radius)",
-// 	  md: "calc(var(--radius) - 2px)",
-// 	  sm: "calc(var(--radius) - 4px)",
-// 	},
