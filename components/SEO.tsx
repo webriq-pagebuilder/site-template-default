14e1d898e61a36d47441e62e7f9fe456c410b445
MarielF WebriQ <info@webriq.com>
2024-02-01 15:12:32 +0800

    Revise implementations

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -1,5 +1,6 @@
 import { seoImageUrl } from "lib/sanity";
 import { DefaultSeoData, SanityImage, SanitySlug } from "types";
+import { BlogJsonLd, PagesJsonLd, ProductJsonLd } from "utils/seo/jsonLd";
 
 type SEOData = {
   pageTitle: string; // page title
@@ -146,68 +147,41 @@ export default SEO;
 export function addSEOJsonLd({ seo, type, defaults, slug, pageData }) {
   if (type === "post") {
     // blog posts
-    return {
-      __html: `{
-          "@context": "https://schema.org",
-          "@type": "BlogPosting",
-          "name": "${seo?.seoTitle ?? pageData?.title}"
-          "description": "${
-            seo?.seoDescription ??
-            blogPostBody(pageData?.excerpt ?? pageData?.body) ??
-            defaults?.description
-          }",
-          "url": "${url}/${slug?.current}",
-          "image": "${seoImageUrl(
-            seo?.seoImage ?? pageData?.mainImage ?? defaults?.image
-          )}",
-          "datePublished": "${pageData?.publishedAt ?? pageData?._createdAt}",
-          "dateModified": "${pageData?._updatedAt}",
-          "author": {
-            "@type": "Person",
-            "name": "${pageData?.authors?.[0]?.name}"
-          }
-        }
-      `,
-    };
+    return BlogJsonLd({
+      title: seo?.seoTitle ?? pageData?.title,
+      description:
+        seo?.seoDescription ??
+        blogPostBody(pageData?.excerpt ?? pageData?.body) ??
+        defaults?.description,
+      url: `${url}/${slug?.current}`,
+      images: seoImageUrl(
+        seo?.seoImage ?? pageData?.mainImage ?? defaults?.image
+      ),
+      authorName: pageData?.authors,
+      publisherName: "WebriQ",
+      publisherLogo: seoImageUrl(seo?.seoImage ?? defaults?.image),
+      dateModified: pageData?._updatedAt,
+      datePublished: pageData?.publishedAt ?? pageData?._updatedAt,
+    });
   } else if (type === "mainProduct") {
     // product pages
-    return {
-      __html: `{
-        "@context": "https://schema.org",
-        "@type": "Product",
-        "name": "${seo?.seoTitle ?? pageData?.title}",
-        "image": "${seoImageUrl(
-          seo?.seoImage ?? pageData?.productInfo?.images?.[0]?.image
-        )}",
-        "description": "${seo?.seoDescription ?? defaults?.description}",
-        "brand": {
-          "@type": "Brand",
-          "name": "WebriQ"
-        },
-        "offers": {
-          "@type": "Offer",
-          "url": "${url}/products/${slug}",
-          "priceCurrency": "USD",
-          "price": "${pageData?.price}",
-          "itemCondition": "https://schema.org/UsedCondition",
-          "availability": "https://schema.org/InStock"
-        }
-      }
-    `,
-    };
+    return ProductJsonLd({
+      productName: seo?.seoTitle ?? pageData?.name,
+      images: seo?.seoImage ?? pageData?.productInfo?.images,
+      url: `${url}/products/${slug}`,
+      brand: "WebriQ",
+      description: seo?.seoDescription ?? defaults?.description,
+      price: pageData?.price,
+      priceCurrency: "USD",
+    });
   } else {
     // default schema type for all pages
-    return {
-      __html: `{
-          "@context": "https://schema.org",
-          "@type": "Corporation",
-          "name": "${seo?.seoTitle ?? pageData?.title}",
-          "description": "${seo?.seoDescription ?? defaults?.description}",
-          "url": "${url}/${slug}",
-          "logo": "${seoImageUrl(seo?.seoImage ?? defaults?.image)}",
-          "contactPoint": ${JSON.stringify(contacts)}
-        }
-      `,
-    };
+    return PagesJsonLd({
+      name: seo?.seoTitle ?? pageData?.title,
+      description: seo?.seoDescription ?? defaults?.description,
+      url: `${url}/${slug}`,
+      logo: seoImageUrl(seo?.seoImage ?? defaults?.image),
+      contactPoint: contacts,
+    });
   }
 }
