46460dbd392518289d67a3d5501bc28c046390c7
Webriq <info@webriq.com>
2024-02-03 15:37:03 +0800

    how it works b

--- components/sections/how_it_works/variant_b.tsx
+++ components/sections/how_it_works/variant_b.tsx
@@ -2,49 +2,55 @@ import React from "react";
 
 import { HowItWorksProps } from ".";
 import { Text } from "components/ui/Text";
+import { Container } from "components/layout/Container";
+import { Flex } from "components/layout/Flex/Flex";
+import { Heading } from "components/ui/Heading";
 
 function VariantB({ subtitle, title, text, steps }: HowItWorksProps) {
   return (
-    <section>
-      <div className="py-20 radius-for-skewed bg-gray-50">
-        <div className="container px-4 mx-auto">
-          <div className="flex flex-wrap items-center">
-            <div className="w-full mb-12 lg:mb-0 lg:w-1/2">
-              <div className="max-w-md mx-20">
-                <span className="font-bold text-brand-primary">{subtitle}</span>
-                <Text type="h1" className="mb-2">
-                  {title}
-                </Text>
-                <div className="max-w-xs">
-                  <p className="leading-loose text-gray-500">{text}</p>
-                </div>
-              </div>
-            </div>
-            <div className="w-full lg:w-1/2">
-              {steps &&
-                steps?.map((step, index) => (
-                  <div
-                    className="flex flex-wrap items-start mb-12 lg:mb-8 lg:ml-10"
-                    key={index}
-                  >
-                    <span className="inline-flex items-center justify-center w-16 h-16 mb-4 text-2xl font-bold rounded bg-brand-secondary-foreground text-brand-primary lg:mb-0 lg:mr-6">
-                      {index + 1}
-                    </span>
-                    <div className="w-full lg:w-3/4">
-                      <p className="mb-4 text-2xl font-bold font-heading">
-                        {step?.title}
-                      </p>
-                      <p className="leading-loose text-gray-500">
-                        {step?.plainText}
-                      </p>
-                    </div>
-                  </div>
-                ))}
-            </div>
+    <section className="py-20 bg-gray-50">
+      <Container>
+        <Flex align="center" justify="between" wrap>
+          <div className="w-full mb-12 lg:mb-0 lg:w-1/2">
+            <Container maxWidth={448}>
+              <Text weight="bold" color="brand-primary">
+                {subtitle}
+              </Text>
+              <Heading className="mb-2">{title}</Heading>
+
+              <Text muted className="leading-loose">
+                {text}
+              </Text>
+            </Container>
           </div>
-        </div>
-      </div>
+
+          <div className="w-full lg:w-1/2">
+            {steps &&
+              steps?.map((step, index) => (
+                <StepItem step={step} index={index} key={step._key} />
+              ))}
+          </div>
+        </Flex>
+      </Container>
     </section>
+  );
+}
+
+function StepItem({ index, step }) {
+  return (
+    <Flex wrap align="start" className="mb-12 " key={index}>
+      <span className="inline-flex items-center justify-center w-16 h-16 mb-4 text-2xl font-bold rounded bg-brand-secondary-foreground text-brand-primary lg:mb-0 lg:mr-6">
+        {index + 1}
+      </span>
+      <div className="w-full lg:w-3/4">
+        <Text weight="bold" className="mb-4 text-2xl">
+          {step?.title}
+        </Text>
+        <Text className="leading-loose" muted>
+          {step?.plainText}
+        </Text>
+      </div>
+    </Flex>
   );
 }
 export default React.memo(VariantB);
