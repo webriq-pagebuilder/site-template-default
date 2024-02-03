329bd9e179b02f99d392c5aacd9141aad5fa1d86
Webriq <info@webriq.com>
2024-02-03 19:21:41 +0800

    portfolio

--- components/sections/portfolio/variant_a.tsx
+++ components/sections/portfolio/variant_a.tsx
@@ -1,9 +1,12 @@
-import React from "react";
-import Image from "next/image";
-import { urlFor } from "lib/sanity";
-import { ConditionalLink } from "components/ui/ConditionalLink";
-import { PortfolioProps } from ".";
+import { Container } from "components/layout/Container";
+import { Flex } from "components/layout/Flex/Flex";
+import { Button } from "components/ui/Button";
+import { Heading } from "components/ui/Heading";
 import { Text } from "components/ui/Text";
+import { urlFor } from "lib/sanity";
+import Image from "next/image";
+import React from "react";
+import { PortfolioProps } from ".";
 
 function VariantA({ caption, title, portfoliosWithCategory }: PortfolioProps) {
   let portfolioLength = 8; //set initial number of portfolios to display for this variant
@@ -17,85 +20,86 @@ function VariantA({ caption, title, portfoliosWithCategory }: PortfolioProps) {
   );
 
   return (
-    <section>
-      <div className="py-20 radius-for-skewed bg-gray-50">
-        <div className="container px-4 mx-auto">
-          <div className="max-w-lg mx-auto mb-8 text-center md:mb-16">
-            {caption && (
-              <span className="font-bold text-brand-primary">{caption}</span>
-            )}
-            {title && <Text className="mb-6 ">{title}</Text>}
-            {portfoliosWithCategory && (
-              <div className="inline-flex flex-wrap py-1 text-sm bg-white rounded">
-                {portfoliosWithCategory?.map((content, index) => (
-                  <button
-                    aria-label={content?.category}
-                    key={index}
-                    className={`mx-auto mb-1 w-auto px-4 py-2 ${
-                      activeTab === content?.category
-                        ? "rounded bg-gray-50 font-bold text-brand-primary shadow transition duration-200 focus:outline-none"
-                        : "rounded font-bold text-gray-700 transition duration-200 hover:bg-brand-secondary-foreground hover:text-brand-primary-foreground hover:shadow focus:outline-none"
-                    }`}
-                    onClick={() => setActiveTab(content?.category)}
-                  >
-                    {content?.category}
-                  </button>
-                ))}
-              </div>
-            )}
-          </div>
-          {portfoliosPerCategory?.[0]?.content && (
-            <div className="flex flex-wrap mb-8 -mx-4">
-              {portfoliosPerCategory?.[0]?.content
-                ?.slice(0, portfolioLength)
-                ?.map((content, index) => (
-                  <div
-                    className="w-full px-4 mb-8 sm:w-1/2 md:w-1/2 lg:w-1/4 xl:w-1/4"
-                    key={index}
-                  >
-                    <div className="relative mx-auto h-[256px] w-[352px] overflow-hidden rounded-lg">
-                      {content?.mainImage?.image && (
-                        <Image
-                          className="object-cover w-full h-full"
-                          src={urlFor(content?.mainImage?.image)}
-                          sizes="100vw"
-                          width={352}
-                          height={256}
-                          alt={
-                            content?.mainImage?.alt ?? `portfolio-image${index}`
-                          }
-                        />
+    <section className="py-20 bg-gray-50">
+      <Container>
+        <Container maxWidth={512} className="mb-8 text-center md:mb-16">
+          {caption && (
+            <Text weight="bold" color="brand-primary">
+              {caption}
+            </Text>
+          )}
+          {title && <Heading className="mb-6">{title}</Heading>}
+          {portfoliosWithCategory && (
+            <div className="inline-flex flex-wrap py-1 text-sm bg-white rounded">
+              {portfoliosWithCategory?.map((content, index) => (
+                <button
+                  aria-label={content?.category}
+                  key={index}
+                  className={`mx-auto mb-1 w-auto px-4 py-2 ${
+                    activeTab === content?.category
+                      ? "rounded bg-gray-50 font-bold text-brand-primary shadow transition duration-200 focus:outline-none"
+                      : "rounded font-bold text-gray-700 transition duration-200 hover:bg-brand-secondary-foreground hover:text-brand-primary-foreground hover:shadow focus:outline-none"
+                  }`}
+                  onClick={() => setActiveTab(content?.category)}
+                >
+                  {content?.category}
+                </button>
+              ))}
+            </div>
+          )}
+        </Container>
+        {portfoliosPerCategory?.[0]?.content && (
+          <Flex wrap className="mb-8">
+            {portfoliosPerCategory?.[0]?.content
+              ?.slice(0, portfolioLength)
+              ?.map((content, index) => (
+                <div
+                  className="w-full px-4 mb-8 sm:w-1/2 lg:w-1/4 "
+                  key={index}
+                >
+                  <div className="relative mx-auto h-[256px] w-[352px] overflow-hidden rounded-lg">
+                    {content?.mainImage?.image && (
+                      <Image
+                        className="object-cover w-full h-full"
+                        src={urlFor(content?.mainImage?.image)}
+                        sizes="100vw"
+                        width={352}
+                        height={256}
+                        alt={
+                          content?.mainImage?.alt ?? `portfolio-image${index}`
+                        }
+                      />
+                    )}
+                    <div className="absolute inset-0 z-10 flex items-center justify-center duration-300 bg-gray-900 rounded-lg opacity-0 hover:opacity-75">
+                      {content?.primaryButton?.label && (
+                        <Button
+                          asLink
+                          variant="outline"
+                          ariaLabel={content?.primaryButton?.label}
+                          link={content?.primaryButton}
+                          className="bg-transparent border-2 border-gray-400 !outline-none hover:border-white hover:opacity-100 text-gray-50 hover:bg-white hover:text-gray-900"
+                        >
+                          {content?.primaryButton?.label}
+                        </Button>
                       )}
-                      <div className="absolute inset-0 z-10 flex items-center justify-center duration-300 bg-gray-900 rounded-lg opacity-0 hover:opacity-75">
-                        {content?.primaryButton?.label && (
-                          <ConditionalLink
-                            variant="outline"
-                            ariaLabel={content?.primaryButton?.label}
-                            link={content?.primaryButton}
-                            className="bg-transparent border-2 border-gray-400 !outline-none hover:border-white hover:opacity-100 text-gray-50 hover:bg-white hover:text-gray-900"
-                          >
-                            {content?.primaryButton?.label}
-                          </ConditionalLink>
-                        )}
-                      </div>
                     </div>
                   </div>
-                ))}
-            </div>
-          )}
-          {portfoliosPerCategory?.[0]?.primaryButton?.label && (
-            <div className="text-center">
-              <ConditionalLink
-                ariaLabel={portfoliosPerCategory?.[0]?.primaryButton?.label}
-                link={portfoliosPerCategory?.[0]?.primaryButton}
-                className="inline-block px-6 py-2 font-bold leading-loose transition duration-200 outline-none rounded-l-xl rounded-t-xl bg-brand-primary hover:bg-brand-primary-foreground text-gray-50"
-              >
-                {portfoliosPerCategory?.[0]?.primaryButton?.label}
-              </ConditionalLink>
-            </div>
-          )}
-        </div>
-      </div>
+                </div>
+              ))}
+          </Flex>
+        )}
+        {portfoliosPerCategory?.[0]?.primaryButton?.label && (
+          <div className="text-center">
+            <Button
+              asLink
+              ariaLabel={portfoliosPerCategory?.[0]?.primaryButton?.label}
+              link={portfoliosPerCategory?.[0]?.primaryButton}
+            >
+              {portfoliosPerCategory?.[0]?.primaryButton?.label}
+            </Button>
+          </div>
+        )}
+      </Container>
     </section>
   );
 }
--- components/sections/portfolio/variant_d.tsx
+++ components/sections/portfolio/variant_d.tsx
@@ -4,6 +4,10 @@ import { urlFor } from "lib/sanity";
 import { ConditionalLink } from "helper";
 import { PortfolioProps } from ".";
 import { Text } from "components/ui/Text";
+import { Container } from "components/layout/Container";
+import { Heading } from "components/ui/Heading";
+import { Flex } from "components/layout/Flex/Flex";
+import { Button } from "components/ui/Button";
 
 function VariantD({ caption, title, portfoliosWithCategory }: PortfolioProps) {
   let portfoliosPerPage = 6,
@@ -18,239 +22,133 @@ function VariantD({ caption, title, portfoliosWithCategory }: PortfolioProps) {
   );
 
   return (
-    <section>
-      <div className="py-20 radius-for-skewed bg-gray-50">
-        <div className="container px-4 mx-auto">
-          <div className="max-w-lg mx-auto mb-8 text-center md:mb-16">
-            {caption && (
-              <span className="font-bold text-brand-primary">{caption}</span>
-            )}
-            {title && (
-              <Text type="h1" className="mb-6">
-                {title}
-              </Text>
-            )}
-            {portfoliosWithCategory && (
-              <div className="inline-flex flex-wrap py-1 text-sm bg-white rounded">
-                {portfoliosWithCategory?.map((content, index) => (
-                  <button
-                    aria-label={content?.category}
-                    key={index}
-                    onClick={() => setActiveTab(content?.category)}
-                    className={`mx-auto mb-1 w-auto px-4 py-2 ${
-                      activeTab === content?.category
-                        ? "rounded bg-gray-50 font-bold text-brand-primary shadow transition duration-200 focus:outline-none"
-                        : "rounded font-bold text-gray-500 transition duration-200 hover:bg-brand-secondary-foreground hover:text-brand-primary-foreground hover:shadow focus:outline-none"
-                    }`}
-                  >
-                    {content?.category}
-                  </button>
-                ))}
-              </div>
-            )}
-          </div>
-          <div className="mb-12 -mx-4 sm:flex">
-            <div className="flex flex-wrap w-full mb-8 lg:mb-0 lg:w-1/2">
-              {portfoliosPerCategory?.[0]?.content
-                ?.slice(count, count + 2)
-                ?.map((content) => (
-                  <div
-                    className="w-full px-4 mb-8 lg:w-1/2"
-                    key={content?._key}
-                  >
-                    {content?.mainImage?.image && (
-                      <div className="relative overflow-hidden rounded">
-                        <Image
-                          className="object-cover w-full h-64"
-                          src={urlFor(content?.mainImage?.image)}
-                          sizes="100vw"
-                          width={352}
-                          height={280}
-                          alt={`portfolio-image-${content?._key}`}
-                        />
-                        <div className="absolute inset-0 z-10 justify-center p-6 duration-300 bg-gray-900 rounded-lg opacity-0 hover:opacity-80">
-                          <div className="max-w-md my-auto text-xs">
-                            <span className="font-bold text-brand-primary-foreground">
-                              {content?.subtitle}
-                            </span>
-                            <h1 className="my-5 font-bold text-white">
-                              {content?.title}
-                            </h1>
-                            <div className="max-w-xs my-5">
-                              <p className="mb-6 text-gray-500">
-                                {content?.description}
-                              </p>
-                              {content?.primaryButton?.label && (
-                                <ConditionalLink
-                                  ariaLabel={content?.primaryButton?.label}
-                                  link={content?.primaryButton}
-                                  className="inline-block px-6 py-2 font-bold leading-loose rounded-l-xl rounded-t-xl bg-brand-primary hover:bg-brand-primary-foreground text-gray-50"
-                                >
-                                  {content?.primaryButton?.label}
-                                </ConditionalLink>
-                              )}
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    )}
-                  </div>
-                ))}
-              {portfoliosPerCategory?.[0]?.content
-                ?.slice(count + 2, count + 3)
-                ?.map((content) => (
-                  <div
-                    className="w-full px-4 mb-8 lg:h-full lg:w-full lg:px-4 xl:px-4"
-                    key={content?._key}
-                  >
-                    {content?.mainImage?.image && (
-                      <div className="relative overflow-hidden rounded">
-                        <Image
-                          className="object-cover w-full h-128"
-                          src={urlFor(content?.mainImage?.image)}
-                          sizes="100vw"
-                          width={352}
-                          height={256}
-                          alt={`portfolio-image-${content?._key}`}
-                        />
-                        <div className="absolute inset-0 z-10 justify-center h-full p-6 duration-300 bg-gray-900 rounded-lg opacity-0 hover:opacity-80">
-                          <div className="max-w-md my-auto text-xs lg:mt-10 lg:text-sm xl:mt-10 xl:text-sm 2xl:mt-10 2xl:text-sm">
-                            <span className="font-bold text-brand-primary-foreground">
-                              {content?.subtitle}
-                            </span>
-                            <h1 className="my-5 font-bold text-white lg:text-4xl xl:text-4xl 2xl:text-4xl">
-                              {content?.title}
-                            </h1>
-                            <div className="max-w-sm my-5">
-                              <p className="mb-6 text-gray-500">
-                                {content?.description}
-                              </p>
-                              {content?.primaryButton?.label && (
-                                <ConditionalLink
-                                  ariaLabel={content?.primaryButton?.label}
-                                  link={content?.primaryButton}
-                                  className="inline-block px-6 py-2 font-bold leading-loose rounded-l-xl rounded-t-xl bg-brand-primary hover:bg-brand-primary-foreground text-gray-50"
-                                >
-                                  {content?.primaryButton?.label}
-                                </ConditionalLink>
-                              )}
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    )}
-                  </div>
-                ))}
-            </div>
-            <div className="w-full lg:w-1/2">
-              {portfoliosPerCategory?.[0]?.content
-                ?.slice(count + 3, count + 4)
-                ?.map((content) => (
-                  <div
-                    className="w-full px-4 mb-8 lg:w-full lg:px-4 xl:w-full xl:px-4"
-                    key={content?._key}
-                  >
-                    {content?.mainImage?.image && (
-                      <div className="relative overflow-hidden rounded">
-                        <Image
-                          className="object-cover w-full h-128"
-                          src={urlFor(content?.mainImage?.image)}
-                          sizes="100vw"
-                          width={352}
-                          height={256}
-                          alt={`portfolio-image-${content?._key}`}
-                        />
-                        <div className="absolute inset-0 z-10 justify-center h-full p-6 duration-300 bg-gray-900 rounded-lg opacity-0 hover:opacity-80 ">
-                          <div className="max-w-md my-auto text-xs lg:mt-10 lg:text-sm xl:mt-10 xl:text-sm 2xl:mt-10 2xl:text-sm">
-                            <span className="font-bold text-brand-primary-foreground">
-                              {content?.subtitle}
-                            </span>
-                            <h1 className="my-5 font-bold text-white lg:text-4xl xl:text-4xl 2xl:text-4xl">
-                              {content?.title}
-                            </h1>
-                            <div className="max-w-xs my-5">
-                              <p className="mb-6 text-gray-500">
-                                {content?.description}
-                              </p>
-                              {content?.primaryButton?.label && (
-                                <ConditionalLink
-                                  ariaLabel={content?.primaryButton?.label}
-                                  link={content?.primaryButton}
-                                  className="inline-block px-6 py-2 font-bold leading-loose rounded-l-xl rounded-t-xl bg-brand-primary hover:bg-brand-primary-foreground text-gray-50"
-                                >
-                                  {content?.primaryButton?.label}
-                                </ConditionalLink>
-                              )}
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    )}
-                  </div>
-                ))}
-              <div className="flex flex-wrap">
-                {portfoliosPerCategory?.[0]?.content
-                  ?.slice(count + 4, portfoliosPerPage)
-                  ?.map((content) => (
-                    <div
-                      className="relative w-full px-4 mb-8 lg:mb-0 lg:w-1/2"
-                      key={content?._key}
-                    >
-                      {content?.mainImage?.image && (
-                        <div className="relative overflow-hidden rounded">
-                          <Image
-                            className="object-cover w-full h-64"
-                            src={urlFor(content?.mainImage?.image)}
-                            width={352}
-                            height={280}
-                            sizes="100vw"
-                            alt={`portfolio-image${content?._key}`}
-                          />
-                          <div className="absolute inset-0 z-10 justify-center h-full p-6 duration-300 bg-gray-900 rounded-lg opacity-0 hover:opacity-80 ">
-                            <div className="max-w-md my-auto text-xs">
-                              <span className="font-bold text-brand-primary-foreground">
-                                {content?.subtitle}
-                              </span>
-                              <h1 className="my-5 font-bold text-white">
-                                {content?.title}
-                              </h1>
-                              <div className="max-w-xs my-5">
-                                <p className="mb-6 text-gray-500">
-                                  {content?.description}
-                                </p>
-                                {content?.primaryButton?.label && (
-                                  <ConditionalLink
-                                    ariaLabel={content?.primaryButton?.label}
-                                    link={content?.primaryButton}
-                                    className="inline-block px-6 py-2 font-bold leading-loose rounded-l-xl rounded-t-xl bg-brand-primary hover:bg-brand-primary-foreground text-gray-50"
-                                  >
-                                    {content?.primaryButton?.label}
-                                  </ConditionalLink>
-                                )}
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      )}
-                    </div>
-                  ))}
-              </div>
-            </div>
-          </div>
-          {portfoliosPerCategory?.[0]?.primaryButton?.label && (
-            <div className="text-center">
-              <ConditionalLink
-                ariaLabel={portfoliosPerCategory?.[0]?.primaryButton?.label}
-                link={portfoliosPerCategory?.[0]?.primaryButton}
-                className="inline-block px-6 py-2 font-bold leading-loose transition duration-200 outline-none rounded-l-xl rounded-t-xl bg-brand-primary hover:bg-brand-primary-foreground text-gray-50"
-              >
-                {portfoliosPerCategory?.[0]?.primaryButton?.label}
-              </ConditionalLink>
+    <section className="py-20 bg-gray-50">
+      <Container>
+        <Container maxWidth={512} className="mb-8 text-center md:mb-16">
+          {caption && (
+            <Text weight="bold" color="brand-primary">
+              {caption}
+            </Text>
+          )}
+          {title && <Heading className="mb-6">{title}</Heading>}
+          {portfoliosWithCategory && (
+            <div className="inline-flex flex-wrap py-1 text-sm bg-white rounded">
+              {portfoliosWithCategory?.map((content, index) => (
+                <button
+                  aria-label={content?.category}
+                  key={index}
+                  onClick={() => setActiveTab(content?.category)}
+                  className={`mx-auto mb-1 w-auto px-4 py-2 ${
+                    activeTab === content?.category
+                      ? "rounded bg-gray-50 font-bold text-brand-primary shadow transition duration-200 focus:outline-none"
+                      : "rounded font-bold text-gray-500 transition duration-200 hover:bg-brand-secondary-foreground hover:text-brand-primary-foreground hover:shadow focus:outline-none"
+                  }`}
+                >
+                  {content?.category}
+                </button>
+              ))}
             </div>
           )}
+        </Container>
+        <div className="mb-12 sm:flex">
+          <Flex wrap className="w-full mb-8 lg:mb-0 lg:w-1/2">
+            {portfoliosPerCategory?.[0]?.content
+              ?.slice(count, count + 2)
+              ?.map((content) => (
+                <ProjectItem size={"sm"} content={content} key={content._key} />
+              ))}
+            {portfoliosPerCategory?.[0]?.content
+              ?.slice(count + 2, count + 3)
+              ?.map((content) => (
+                <ProjectItem size={"lg"} content={content} key={content._key} />
+              ))}
+          </Flex>
+          <div className="w-full lg:w-1/2">
+            {portfoliosPerCategory?.[0]?.content
+              ?.slice(count + 3, count + 4)
+              ?.map((content) => (
+                <ProjectItem size={"lg"} content={content} key={content._key} />
+              ))}
+            <div className="flex flex-wrap">
+              {portfoliosPerCategory?.[0]?.content
+                ?.slice(count + 4, portfoliosPerPage)
+                ?.map((content) => (
+                  <ProjectItem
+                    size={"sm"}
+                    content={content}
+                    key={content._key}
+                  />
+                ))}
+            </div>
+          </div>
         </div>
-      </div>
+        {portfoliosPerCategory?.[0]?.primaryButton?.label && (
+          <div className="text-center">
+            <Button
+              asLink
+              ariaLabel={portfoliosPerCategory?.[0]?.primaryButton?.label}
+              link={portfoliosPerCategory?.[0]?.primaryButton}
+            >
+              {portfoliosPerCategory?.[0]?.primaryButton?.label}
+            </Button>
+          </div>
+        )}
+      </Container>
     </section>
+  );
+}
+
+function ProjectItem({ size, content }) {
+  return (
+    <div
+      className={`w-full px-4 mb-8 ${size === "lg" ? "w-full" : "lg:w-1/2"}`}
+    >
+      {content?.mainImage?.image && (
+        <div className="relative overflow-hidden rounded">
+          <Image
+            className={`"object-cover w-full ${
+              size === "lg" ? "h-128" : "h-64"
+            }`}
+            src={urlFor(content?.mainImage?.image)}
+            sizes="100vw"
+            width={352}
+            height={280}
+            alt={`portfolio-image-${content?._key}`}
+          />
+          <div className="absolute inset-0 z-10 justify-center p-6 duration-300 bg-gray-900 rounded-lg opacity-0 hover:opacity-80">
+            <div className="max-w-md my-auto text-xs">
+              <Text className="text-sm" weight="bold" color="brand-primary">
+                {content?.subtitle}
+              </Text>
+              <Heading
+                weight="bold"
+                className={`my-5 text-white ${
+                  size === "lg"
+                    ? "md:text-4xl"
+                    : "text-sm md:text-sm lg:text-sm"
+                }`}
+              >
+                {content?.title}
+              </Heading>
+              <div className="max-w-xs my-5">
+                <Text muted className="mb-6 text-xs">
+                  {content?.description}
+                </Text>
+                {content?.primaryButton?.label && (
+                  <Button
+                    asLink
+                    ariaLabel={content?.primaryButton?.label}
+                    link={content?.primaryButton}
+                  >
+                    {content?.primaryButton?.label}
+                  </Button>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
 
