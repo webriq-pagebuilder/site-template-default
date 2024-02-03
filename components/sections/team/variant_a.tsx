5a3a77dfa9b4cf735f9905bd29f447966d19d48a
Webriq <info@webriq.com>
2024-02-03 22:19:39 +0800

    team

--- components/sections/team/variant_a.tsx
+++ components/sections/team/variant_a.tsx
@@ -1,51 +1,53 @@
-import React from "react";
-import Image from "next/image";
-import { urlFor } from "lib/sanity";
-import { TeamsProps } from ".";
-import { Card } from "components/ui/Card";
+import { Container } from "components/layout/Container";
+import { Flex } from "components/layout/Flex/Flex";
 import { Avatar } from "components/ui/Avatar";
+import { Card } from "components/ui/Card";
+import { Heading } from "components/ui/Heading";
 import { Text } from "components/ui/Text";
+import { urlFor } from "lib/sanity";
+import React from "react";
+import { TeamsProps } from ".";
 
 function VariantA({ caption, title, team }: TeamsProps) {
   return (
-    <section>
-      <div className="py-20 radius-for-skewed bg-gray-50">
-        <div className="container px-4 mx-auto">
-          <div className="max-w-xl mx-auto mb-10 text-center">
-            {caption && (
-              <span className="font-bold text-brand-primary">{caption}</span>
-            )}
-            {title && <Text type="h1">{title}</Text>}
-          </div>
-          <div className="flex flex-wrap">
-            {team &&
-              team?.map((member) => (
-                <div
-                  className="w-full px-3 mb-6 md:w-1/2 lg:w-1/3"
-                  key={member?.name}
-                >
-                  <Card className="py-24 text-center ">
-                    {member?.mainImage?.image && (
-                      <Avatar
-                        className="mx-auto border-0"
-                        size={96}
-                        alt={
-                          member?.mainImage?.alt ??
-                          `team-member-${member?.name}-profile-image`
-                        }
-                        src={urlFor(member?.mainImage?.image)}
-                      />
-                    )}
-                    <h1 className="mb-2 text-2xl font-bold font-heading">
-                      {member?.name}
-                    </h1>
-                    <Text muted>{member?.jobTitle}</Text>
-                  </Card>
-                </div>
-              ))}
-          </div>
-        </div>
-      </div>
+    <section className="py-20 bg-gray-50">
+      <Container>
+        <Container maxWidth={576} className="mb-10 text-center">
+          {caption && (
+            <Text weight="bold" color="brand-primary">
+              {caption}
+            </Text>
+          )}
+          {title && <Heading>{title}</Heading>}
+        </Container>
+        <Flex wrap>
+          {team &&
+            team?.map((member) => (
+              <div
+                className="w-full px-3 mb-6 md:w-1/2 lg:w-1/3"
+                key={member?.name}
+              >
+                <Card className="py-24 text-center ">
+                  {member?.mainImage?.image && (
+                    <Avatar
+                      className="mx-auto border-0"
+                      size={96}
+                      alt={
+                        member?.mainImage?.alt ??
+                        `team-member-${member?.name}-profile-image`
+                      }
+                      src={urlFor(member?.mainImage?.image)}
+                    />
+                  )}
+                  <Heading type="h3" className="mb-2 text-2xl lg:text-2xl">
+                    {member?.name}
+                  </Heading>
+                  <Text muted>{member?.jobTitle}</Text>
+                </Card>
+              </div>
+            ))}
+        </Flex>
+      </Container>
     </section>
   );
 }
--- components/sections/team/variant_b.tsx
+++ components/sections/team/variant_b.tsx
@@ -4,6 +4,8 @@ import { urlFor } from "lib/sanity";
 import { TeamsProps } from ".";
 import { Card } from "components/ui/Card";
 import { Text } from "components/ui/Text";
+import { Container } from "components/layout/Container";
+import { Flex } from "components/layout/Flex/Flex";
 
 function VariantB({ team }: TeamsProps) {
   const [activeTab, setActiveTab] = React.useState(team?.[0]?.name); // default active tab is the first tab
@@ -11,66 +13,68 @@ function VariantB({ team }: TeamsProps) {
   const filterMember = team?.filter((member) => member?.name === activeTab);
 
   return (
-    <section>
+    <section className="py-20 bg-gray-50">
       {team && (
-        <div className="py-20 radius-for-skewed bg-gray-50">
-          <div className="container px-4 mx-auto">
-            <div className="flex flex-wrap items-center -mx-3">
-              <div className="w-full px-3 mb-8 lg:mb-0 lg:w-1/3">
-                <ul className="flex flex-row flex-wrap justify-center space-x-6 lg:flex-col lg:justify-start lg:space-x-0">
-                  {team &&
-                    team?.map((item) => (
-                      <li key={item?.name}>
-                        <button
-                          aria-label={item.name}
-                          className={`mb-4 text-2xl lg:text-4xl ${
-                            item?.name === activeTab
-                              ? "text-gray-900"
-                              : "text-gray-500"
-                          } font-bold hover:text-gray-500 focus:outline-none`}
-                          onClick={() => setActiveTab(item?.name)}
-                        >
-                          {item.name}
-                        </button>
-                      </li>
-                    ))}
-                </ul>
-              </div>
-              {team && (
-                <div className="w-full px-3 lg:w-2/3">
-                  {filterMember?.map((member, index) => (
-                    <Card className="flex p-6 bg-white" key={index}>
-                      {member?.mainImage?.image && (
-                        <Image
-                          className="object-cover overflow-hidden rounded-lg"
-                          src={urlFor(member?.mainImage?.image)}
-                          sizes="100vw"
-                          width={329}
-                          height={494}
-                          alt={
-                            member?.mainImage?.alt ??
-                            `team-member-${member?.name}-profile-image`
-                          }
-                        />
-                      )}
-                      <div className="order-last w-1/2 pt-6 pl-6 mt-6">
-                        <p className="text-2xl font-bold font-heading">
-                          {member?.name}
-                        </p>
-                        <Text className="mb-6" muted>
-                          {member?.jobTitle}
-                        </Text>
-                        <Text className="mb-6 text-justify " muted>
-                          {member?.plainText}
-                        </Text>
-                      </div>
-                    </Card>
+        <Container>
+          <Flex wrap align="center">
+            <div className="w-full px-3 mb-8 lg:mb-0 lg:w-1/3">
+              <Flex
+                as="ul"
+                wrap
+                className="flex-row justify-center space-x-6 lg:flex-col lg:justify-start lg:space-x-0"
+              >
+                {team &&
+                  team?.map((item) => (
+                    <li key={item?.name}>
+                      <button
+                        aria-label={item.name}
+                        className={`mb-4 text-2xl lg:text-4xl ${
+                          item?.name === activeTab
+                            ? "text-gray-900"
+                            : "text-gray-500"
+                        } font-bold hover:text-gray-500 focus:outline-none`}
+                        onClick={() => setActiveTab(item?.name)}
+                      >
+                        {item.name}
+                      </button>
+                    </li>
                   ))}
-                </div>
-              )}
+              </Flex>
             </div>
-          </div>
-        </div>
+            {team && (
+              <div className="w-full px-3 lg:w-2/3">
+                {filterMember?.map((member, index) => (
+                  <Card className="flex p-6 bg-white" key={index}>
+                    {member?.mainImage?.image && (
+                      <Image
+                        className="object-cover overflow-hidden rounded-lg"
+                        src={urlFor(member?.mainImage?.image)}
+                        sizes="100vw"
+                        width={329}
+                        height={494}
+                        alt={
+                          member?.mainImage?.alt ??
+                          `team-member-${member?.name}-profile-image`
+                        }
+                      />
+                    )}
+                    <div className="order-last w-1/2 pt-6 pl-6 mt-6">
+                      <Text weight="bold" className="text-2xl ">
+                        {member?.name}
+                      </Text>
+                      <Text className="mb-6" muted>
+                        {member?.jobTitle}
+                      </Text>
+                      <Text className="mb-6 text-justify " muted>
+                        {member?.plainText}
+                      </Text>
+                    </div>
+                  </Card>
+                ))}
+              </div>
+            )}
+          </Flex>
+        </Container>
       )}
     </section>
   );
