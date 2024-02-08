ade618d0264e9bbb2bcaf9d6aa6e23cd29c31e60
Webriq <info@webriq.com>
2024-02-08 09:48:06 +0800

    chore: Add grid component

--- components/layout/Flex/Flex.tsx
+++ components/layout/Flex/Flex.tsx
@@ -36,7 +36,36 @@ export function Flex({
   as = "div",
   ...props
 }: FlexProps) {
-  const classes = `flex flex-${direction} items-${align} justify-${justify} ${
+  const directionVariants = {
+    row: "flex-row",
+    col: "flex-col",
+    "row-reverse": "flex-row-reverse",
+    "col-reverse": "flex-col-reverse",
+  };
+
+  const alignVariants = {
+    start: "items-start",
+    end: "items-end",
+    baseline: "items-baseline",
+    stretch: "items-stretch",
+    center: "items-center",
+  };
+
+  const justifyVariants = {
+    normal: "justify-normal",
+    start: "justify-start",
+    end: "justify-end",
+    center: "justify-center",
+    between: " justify-between",
+    around: "justify-around",
+    evenly: "justify-evenly",
+    stretch: "justify-stretch",
+  };
+
+  const directionClass = directionVariants[direction];
+  const alignClass = alignVariants[align];
+  const justifyClass = justifyVariants[justify];
+  const classes = `flex ${directionClass} ${alignClass} ${justifyClass} ${
     wrap ? "flex-wrap" : ""
   } gap-${gap ? gap : 0} `;
 
