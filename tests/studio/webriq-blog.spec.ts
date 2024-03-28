6bcd1c6574f92e8fe892e6bed9a670bc641b4fc7
MarielF WebriQ <info@webriq.com>
2024-03-28 13:58:51 +0800

    Fixes for webriq-blog test

--- tests/studio/webriq-blog.spec.ts
+++ tests/studio/webriq-blog.spec.ts
@@ -19,123 +19,106 @@ test.beforeEach(async ({ page }) => {
   await page.evaluate(autologin_studio, { token, projectId });
 });
 
-// TODO: WIP
-test.describe("Features", () => {
-  // Applies to all tests in this group.
-  test.describe.configure({ timeout: 120000 });
+test("Create new author, category and post", async ({ page }) => {
+  // Navigate to the studio URL
+  await page.goto(`${NEXT_PUBLIC_SANITY_STUDIO_URL}`);
+
+  test.setTimeout(300000);
 
   // CREATE AUTHOR
-  test("Create New Author", async ({ page }) => {
-    await page.goto(`${NEXT_PUBLIC_SANITY_STUDIO_URL}`);
-
-    await page.getByRole("link", { name: "Blog" }).click();
-    await page.getByRole("button", { name: "Create", exact: true }).click();
-    await page.getByRole("menuitem", { name: "Author" }).click();
-    await page.getByTestId("field-name").getByTestId("string-input").click();
-    await page
-      .getByTestId("field-name")
-      .getByTestId("string-input")
-      .fill(newAuthor);
-    await page.getByRole("button", { name: "Generate" }).click();
-    await page.getByLabel("Bio").click();
-    await page.getByLabel("Bio").fill("This is a sample author bio.");
-    await page.getByTestId("action-[object Object]").click({ force: true });
-
-    // check if the author was created, find by name
-    await page.getByRole("link", { name: "Blog" }).click({ force: true });
-    await page.getByRole("button", { name: "Authors", exact: true }).click();
-    await expect(
-      page.locator("div").filter({ hasText: newAuthor }).nth(1)
-    ).toHaveCount(1, { timeout: 5000 });
-  });
+  await page.getByRole("link", { name: "Blog" }).click();
+  await page.getByRole("button", { name: "Create", exact: true }).click();
+  await page.getByRole("menuitem", { name: "Author" }).click();
+  await page.getByTestId("field-name").getByTestId("string-input").click();
+  await page
+    .getByTestId("field-name")
+    .getByTestId("string-input")
+    .fill(newAuthor);
+  await page.getByRole("button", { name: "Generate" }).click();
+  await page.getByLabel("Bio").click();
+  await page.getByLabel("Bio").fill("This is a sample author bio.");
+  await page.getByTestId("action-[object Object]").click({ force: true }); // publish document
+  await expect(
+    page
+      .locator("[aria-label='Review changes']")
+      .filter({ hasText: "just now" })
+  ).toHaveCount(1, { timeout: 10000 });
 
   // CREATE CATEGORY
-  test("Create New Category", async ({ page }) => {
-    await page.goto(`${NEXT_PUBLIC_SANITY_STUDIO_URL}`);
+  await page.getByRole("link", { name: "Blog" }).click();
+  await page
+    .getByRole("button", { name: "Create", exact: true })
+    .click({ force: true });
+  await page.getByRole("menuitem", { name: "Category" }).click();
+  await page.getByTestId("string-input").click();
+  await page.getByTestId("string-input").fill(newCategory);
+  await page.getByLabel("Description").click();
+  await page.getByLabel("Description").fill("This is a sample category.");
+  await page.getByTestId("action-[object Object]").click({ force: true }); // publish document
+  await expect(
+    page
+      .locator("[aria-label='Review changes']")
+      .filter({ hasText: "just now" })
+  ).toHaveCount(1, { timeout: 10000 });
 
-    await page.getByRole("link", { name: "Blog" }).click();
-    await page.getByRole("button", { name: "Create", exact: true }).click();
-    await page.getByRole("menuitem", { name: "Category" }).click();
-    await page.getByTestId("field-name").getByTestId("string-input").click();
-    await page
-      .getByTestId("field-name")
-      .getByTestId("string-input")
-      .fill(newCategory);
-    await page.getByLabel("Description").click();
-    await page.getByLabel("Description").fill("This is a sample category.");
-    await page.getByTestId("action-[object Object]").click({ force: true });
+  // CREATE POST
+  await page.getByRole("link", { name: "Blog" }).click();
+  await page
+    .getByRole("button", { name: "Create", exact: true })
+    .click({ force: true });
+  await page.getByRole("menuitem", { name: "Post" }).click();
+  await page.getByTestId("string-input").click();
+  await page.getByTestId("string-input").fill(newBlogPost);
+  await page.getByRole("button", { name: "Generate" }).click();
+  await page.getByLabel("Excerpt").click();
+  await page.getByLabel("Excerpt").fill("Sample excerpt");
+  await page
+    .getByTestId("field-authors")
+    .getByRole("button", { name: "Add item" })
+    .click({ force: true });
+  await page.getByTestId("autocomplete").click();
+  await page.getByTestId("autocomplete").fill("New Author");
+  await page.locator("button:has-text('New Author -')").first().click();
+  await page
+    .getByTestId("field-categories")
+    .getByRole("button", { name: "Add item" })
+    .click();
+  await page.getByTestId("autocomplete").click();
+  await page.getByTestId("autocomplete").fill("New Category");
+  await page.locator("button:has-text('New Category -')").first().click();
+  await page.getByTestId("date-input").click();
+  await page.getByTestId("select-date-button").click();
+  await page
+    .getByRole("button", { name: "Set to current time" })
+    .click({ force: true });
+  await page.getByTestId("select-date-button").click();
+  await page.getByText("Click to activate").click({ force: true });
+  await page
+    .getByTestId("scroll-container")
+    .getByRole("textbox")
+    .fill("This is a sample blog post content.");
+  await page.getByTestId("action-[object Object]").click({ force: true }); // publish document
+  await expect(
+    page
+      .locator("[aria-label='Review changes']")
+      .filter({ hasText: "just now" })
+  ).toHaveCount(1, { timeout: 10000 });
+});
 
-    // check if the category was created, find by name
-    await page.getByRole("link", { name: "Blog" }).click({ force: true });
-    await page.getByRole("button", { name: "Categories", exact: true }).click();
-    await expect(
-      page.locator("div").filter({ hasText: newCategory }).nth(1)
-    ).toHaveCount(1, { timeout: 5000 });
-  });
+test.afterEach(async ({ page }) => {
+  await page.getByRole("link", { name: "Blog" }).click();
+  await page.getByRole("tab", { name: "Posts", exact: true }).click();
+  await expect(
+    page.getByText(newBlogPost, { exact: true }).first()
+  ).toHaveCount(1);
 
-  // CREATE POST AND LINK NEW AUTHOR AND CATEGORY
-  test("Create New Post", async ({ page }) => {
-    await page.goto(`${NEXT_PUBLIC_SANITY_STUDIO_URL}`);
+  await page.getByRole("tab", { name: "Authors", exact: true }).click();
+  await expect(page.getByText(newAuthor, { exact: true }).first()).toHaveCount(
+    1
+  );
 
-    await page.getByRole("link", { name: "Blog" }).click();
-    await page.getByRole("button", { name: "Create", exact: true }).click();
-    await page.getByRole("menuitem", { name: "Post" }).click();
-    await page.getByTestId("field-name").getByTestId("string-input").click();
-    await page
-      .getByTestId("field-name")
-      .getByTestId("string-input")
-      .fill(newBlogPost);
-    await page.getByRole("button", { name: "Generate" }).click();
-
-    // add excerpt
-    await page.getByLabel("Excerpt").click();
-    await page.getByLabel("Excerpt").fill("Sample excerpt");
-
-    // link author
-    await page
-      .getByTestId("field-authors")
-      .getByRole("button", { name: "Add item" })
-      .click({ force: true });
-    await page.getByTestId("autocomplete").click();
-    await page.getByTestId("autocomplete").fill(newAuthor);
-    await page.getByRole("button", { name: newAuthor }).click({ force: true });
-
-    // TODO: add main image
-    // await page.getByLabel("Main image").click()
-    // await page.getByTestId("file-input-browse-button").click();
-    // await page
-    //   .getByRole("button", { name: "Insert image post Upload" })
-    //   .click();
-
-    // link category
-    await page
-      .getByTestId("field-categories")
-      .getByRole("button", { name: "Add item" })
-      .click();
-    await page.getByTestId("autocomplete").click();
-    await page.getByTestId("autocomplete").fill(newCategory);
-    await page.getByRole("button", { name: newAuthor }).click({ force: true });
-
-    // add date
-    await page.getByTestId("date-input").click();
-    await page.getByTestId("select-date-button").click();
-    await page
-      .getByRole("button", { name: "Set to current time" })
-      .click({ force: true });
-    await page
-      .getByTestId("scroll-container")
-      .getByRole("textbox")
-      .fill("This is a sample blog post content.");
-
-    // add blog content
-    await page.getByLabel("Body").click();
-    await page.getByText("Click to activate").click({ force: true });
-
-    // check if the category was created, find by name
-    await page.getByRole("link", { name: "Blog" }).click({ force: true });
-    await page.getByRole("button", { name: "Posts", exact: true }).click();
-    await expect(
-      page.locator("div").filter({ hasText: newBlogPost }).nth(1)
-    ).toHaveCount(1, { timeout: 5000 });
-  });
+  await page.getByRole("tab", { name: "Categories", exact: true }).click();
+  await expect(
+    page.getByText(newCategory, { exact: true }).first()
+  ).toHaveCount(1);
 });
