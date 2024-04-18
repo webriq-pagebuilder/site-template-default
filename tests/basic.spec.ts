853637b1552d67512a44ad5b55922d46ee50e19e
Roseller Enriquez <rosellerenriquez@Roseller-Enriquez.local>
2024-04-18 15:44:57 +0800

    initial commit for signin signup variants

--- tests/basic.spec.ts
+++ tests/basic.spec.ts
@@ -1,12 +1,17 @@
 import { test, expect, type Page } from "@playwright/test";
-import { autologin_studio, createNewPage, expectDocumentPublished, navigateToPage } from "./utils/index";
+import {
+  autologin_studio,
+  createNewPage,
+  expectDocumentPublished,
+  navigateToPage,
+} from "./utils/index";
 import { NEXT_PUBLIC_SITE_URL } from "studio/config";
 
 let page: Page;
 const newPageTitle = "New Page - " + new Date().getTime();
 
 test.beforeAll(async ({ browser }) => {
-  page= await browser.newPage();
+  page = await browser.newPage();
 
   //navigate to the studio
   await page.goto(`${NEXT_PUBLIC_SITE_URL}`);
@@ -22,14 +27,24 @@ test.describe("Main Workflow", () => {
 
   //PUBLISH PAGES
   test("Test to Publish a Page and Open Live URL", async () => {
-    await navigateToPage(page)
-    await createNewPage(page, newPageTitle, 'Navigation')
-    
-    await page.getByTestId("field-label").getByTestId("string-input").click({ force: true });
-    await page.getByTestId("field-label").getByTestId("string-input").fill("Navigation New Page Variant A");
+    await navigateToPage(page);
+    await createNewPage(page, newPageTitle, "Navigation");
 
-    await page.getByTestId("field-variant").getByRole("img").first().click({ force: true });
-    
+    await page
+      .getByTestId("field-label")
+      .getByTestId("string-input")
+      .click({ force: true });
+    await page
+      .getByTestId("field-label")
+      .getByTestId("string-input")
+      .fill("Navigation New Page Variant A");
+
+    await page
+      .getByTestId("field-variant")
+      .getByRole("img")
+      .first()
+      .click({ force: true });
+
     await expectDocumentPublished(page);
     await expect(page.getByRole("link", { name: newPageTitle })).toBeVisible();
 
@@ -42,7 +57,10 @@ test.describe("Main Workflow", () => {
     const openUrlPage = await pagePromise;
 
     // Wait for the element to become visible or hidden with a longer timeout
-    const sectionCount = await page.locator("div").filter({ hasText: /^No items$/ }).count();
+    const sectionCount = await page
+      .locator("div")
+      .filter({ hasText: /^No items$/ })
+      .count();
     if (sectionCount > 0) {
       // If the section is found, expect the Empty Page element to be visible
       await expect(openUrlPage.getByText("Empty Page")).toBeVisible({
@@ -61,13 +79,17 @@ test.describe("Main Workflow", () => {
     const duplicatePageName = `Dupe Page ` + new Date().getTime();
     await navigateToPage(page);
 
-    await page.getByPlaceholder('Search list').click({ force: true })
-    await page.getByPlaceholder('Search list').fill(newPageTitle)
-    await page.waitForSelector(`a:has-text("${newPageTitle}")`, { state: 'visible' });
+    await page.getByPlaceholder("Search list").click({ force: true });
+    await page.getByPlaceholder("Search list").fill(newPageTitle);
+    await page.waitForSelector(`a:has-text("${newPageTitle}")`, {
+      state: "visible",
+    });
 
     await page.getByRole("link", { name: newPageTitle }).click({ force: true });
-    await page.waitForSelector(`a:has-text("${newPageTitle}")`, { state: 'visible' });
-    await page.getByLabel('Clear').click({ force: true })
+    await page.waitForSelector(`a:has-text("${newPageTitle}")`, {
+      state: "visible",
+    });
+    await page.getByLabel("Clear").click({ force: true });
     await page.waitForTimeout(3000);
     await page.getByTestId("action-menu-button").click({ force: true });
     await page.getByTestId("action-Duplicate").click({ force: true });
@@ -75,18 +97,36 @@ test.describe("Main Workflow", () => {
     await page.getByPlaceholder("Copy of new Page -").fill(duplicatePageName);
     await page.getByRole("button", { name: "Duplicate" }).click();
 
-    await expect(page.locator('[id="__next"]').getByRole("alert").locator("div").nth(1)).toBeVisible();
-    await page.waitForSelector(`a:has-text("${duplicatePageName}")`, { state: 'visible' });
-    await expect(page.getByRole("link", { name: duplicatePageName })).toBeVisible();
-    await page.getByRole("link", { name: duplicatePageName }).click({ force: true });
+    await expect(
+      page.locator('[id="__next"]').getByRole("alert").locator("div").nth(1)
+    ).toBeVisible();
+    await page.waitForSelector(`a:has-text("${duplicatePageName}")`, {
+      state: "visible",
+    });
+    await expect(
+      page.getByRole("link", { name: duplicatePageName })
+    ).toBeVisible();
+    await page
+      .getByRole("link", { name: duplicatePageName })
+      .click({ force: true });
     await page.waitForTimeout(5000);
-    await page.getByTestId('action-[object Object]').click({ force: true });
-    await expect(page.getByTestId('review-changes-button')).toBeHidden({ timeout: 150000 })
-    await expect(page.locator('[id="__next"]').getByRole("alert").locator("div").filter({ hasText: "The document was published" }).nth(1)).toBeVisible({ timeout: 150000 });
-
+    await page.getByTestId("action-[object Object]").click({ force: true });
+    await expect(page.getByTestId("review-changes-button")).toBeHidden({
+      timeout: 150000,
+    });
+    await expect(
+      page
+        .locator('[id="__next"]')
+        .getByRole("alert")
+        .locator("div")
+        .filter({ hasText: "The document was published" })
+        .nth(1)
+    ).toBeVisible({ timeout: 150000 });
 
     //Open Live URL
-    await page.getByRole("link", { name: duplicatePageName }).click({ force: true });
+    await page
+      .getByRole("link", { name: duplicatePageName })
+      .click({ force: true });
     await page.waitForTimeout(5000);
 
     const pagePromise = page.waitForEvent("popup");
@@ -94,7 +134,10 @@ test.describe("Main Workflow", () => {
     const openUrlPage = await pagePromise;
 
     // Wait for the element to become visible or hidden with a longer timeout
-    const sectionCount = await page.locator("div").filter({ hasText: /^No items$/ }).count();
+    const sectionCount = await page
+      .locator("div")
+      .filter({ hasText: /^No items$/ })
+      .count();
     if (sectionCount > 0) {
       // If the section is found, expect the Empty Page element to be visible
       await expect(openUrlPage.getByText("Empty Page")).toBeVisible({
@@ -111,13 +154,17 @@ test.describe("Main Workflow", () => {
   //Launch Inline Editing - Edit and Close button function
   test("Open Inline Editing and Edit Texts should display in realtime", async () => {
     await navigateToPage(page);
-    await page.getByPlaceholder('Search list').click({ force: true })
-    await page.getByPlaceholder('Search list').fill(newPageTitle)
-    await page.waitForSelector(`a:has-text("${newPageTitle}")`, { state: 'visible' });
+    await page.getByPlaceholder("Search list").click({ force: true });
+    await page.getByPlaceholder("Search list").fill(newPageTitle);
+    await page.waitForSelector(`a:has-text("${newPageTitle}")`, {
+      state: "visible",
+    });
 
     await page.getByRole("link", { name: newPageTitle }).click({ force: true });
-    await page.waitForSelector(`a:has-text("${newPageTitle}")`, { state: 'visible' });
-    await page.getByLabel('Clear').click({ force: true })
+    await page.waitForSelector(`a:has-text("${newPageTitle}")`, {
+      state: "visible",
+    });
+    await page.getByLabel("Clear").click({ force: true });
     await page.waitForTimeout(5000);
 
     const launchInlineEditing = page.waitForEvent("popup");
@@ -126,7 +173,9 @@ test.describe("Main Workflow", () => {
     await page.getByLabel("Launch Inline Editing").click();
     const inlineEditPage = await launchInlineEditing;
     await inlineEditPage.locator("#navigation").click(); //Edit button
-    await expect(inlineEditPage.locator(".react-split > div:nth-child(2)")).toBeVisible();
+    await expect(
+      inlineEditPage.locator(".react-split > div:nth-child(2)")
+    ).toBeVisible();
 
     const routes = [
       {
@@ -154,63 +203,106 @@ test.describe("Main Workflow", () => {
         currentRoute: "Testimonials",
         updatedRoute: "Testimonials Test",
       },
-    ]
-    
+    ];
+
     //Edit Routes
     for (const route of routes) {
-      await inlineEditPage.getByRole('button', { name: route.name }).click();
-      await expect(inlineEditPage.getByLabel('Edit Link')).toBeVisible();
-      await inlineEditPage.locator(`input[value="${route.currentRoute}"]`).click();
-      await inlineEditPage.locator(`input[value="${route.currentRoute}"]`).fill(route.updatedRoute);
-      await inlineEditPage.getByLabel('Close dialog').click();
+      await inlineEditPage.getByRole("button", { name: route.name }).click();
+      await expect(inlineEditPage.getByLabel("Edit Link")).toBeVisible();
+      await inlineEditPage
+        .locator(`input[value="${route.currentRoute}"]`)
+        .click();
+      await inlineEditPage
+        .locator(`input[value="${route.currentRoute}"]`)
+        .fill(route.updatedRoute);
+      await inlineEditPage.getByLabel("Close dialog").click();
     }
 
-    await expect(inlineEditPage.locator('[data-testid="review-changes-button"]').filter({ hasText: "Saved!" })).toBeVisible({ timeout: 150000 });
+    await expect(
+      inlineEditPage
+        .locator('[data-testid="review-changes-button"]')
+        .filter({ hasText: "Saved!" })
+    ).toBeVisible({ timeout: 150000 });
     await inlineEditPage.getByTestId("action-Save").click({ force: true });
-    await expect(inlineEditPage.getByTestId('review-changes-button')).toBeHidden({ timeout: 150000 });
-    await expect(inlineEditPage.locator('[id="__next"]').getByRole("alert").locator("div").filter({ hasText: "The document was published" }).nth(1)).toBeVisible({ timeout: 150000 });
+    await expect(
+      inlineEditPage.getByTestId("review-changes-button")
+    ).toBeHidden({ timeout: 150000 });
+    await expect(
+      inlineEditPage
+        .locator('[id="__next"]')
+        .getByRole("alert")
+        .locator("div")
+        .filter({ hasText: "The document was published" })
+        .nth(1)
+    ).toBeVisible({ timeout: 150000 });
 
     await inlineEditPage.locator("#navigation").click({ force: true }); //Close Button
-    await expect(inlineEditPage.locator(".react-split > div:nth-child(2)")).toBeHidden();
-    
+    await expect(
+      inlineEditPage.locator(".react-split > div:nth-child(2)")
+    ).toBeHidden();
+
     //Expect updated route names
     for (const route of routes) {
-      await expect(inlineEditPage.getByRole('link', { name: route.updatedRoute })).toBeVisible();
+      await expect(
+        inlineEditPage.getByRole("link", { name: route.updatedRoute })
+      ).toBeVisible();
     }
   });
 
   test("Test with no Section should display Empty Page", async () => {
     await navigateToPage(page);
-    await page.getByPlaceholder('Search list').click({ force: true })
-    await page.getByPlaceholder('Search list').fill(newPageTitle)
-    await page.waitForSelector(`a:has-text("${newPageTitle}")`, { state: 'visible' });
+    await page.getByPlaceholder("Search list").click({ force: true });
+    await page.getByPlaceholder("Search list").fill(newPageTitle);
+    await page.waitForSelector(`a:has-text("${newPageTitle}")`, {
+      state: "visible",
+    });
 
     await page.getByRole("link", { name: newPageTitle }).click({ force: true });
-    await page.waitForSelector(`a:has-text("${newPageTitle}")`, { state: 'visible' });
-    await page.getByLabel('Clear').click({ force: true })
+    await page.waitForSelector(`a:has-text("${newPageTitle}")`, {
+      state: "visible",
+    });
+    await page.getByLabel("Clear").click({ force: true });
     await page.waitForTimeout(3000);
-    await expect(page.getByText('Loading document')).toBeHidden({ timeout: 150000 });
-    await page.getByTestId('field-sections').getByRole('button').nth(1).click();
-    await page.getByRole('menuitem', { name: 'Remove' }).click();
+    await expect(page.getByText("Loading document")).toBeHidden({
+      timeout: 150000,
+    });
+    await page.getByTestId("field-sections").getByRole("button").nth(1).click();
+    await page.getByRole("menuitem", { name: "Remove" }).click();
 
-    await expect(page.getByTestId('review-changes-button')).toBeVisible({ timeout: 150000 });
-    await page.getByTestId('action-[object Object]').click({ force: true });
-    await expect(page.getByTestId('review-changes-button')).toBeHidden({ timeout: 150000 });
-    await expect(page.locator('[id="__next"]').getByRole('alert').locator('div').filter({ hasText: 'The document was published' }).nth(1)).toBeVisible({ timeout: 150000 });
+    await expect(page.getByTestId("review-changes-button")).toBeVisible({
+      timeout: 150000,
+    });
+    await page.getByTestId("action-[object Object]").click({ force: true });
+    await expect(page.getByTestId("review-changes-button")).toBeHidden({
+      timeout: 150000,
+    });
+    await expect(
+      page
+        .locator('[id="__next"]')
+        .getByRole("alert")
+        .locator("div")
+        .filter({ hasText: "The document was published" })
+        .nth(1)
+    ).toBeVisible({ timeout: 150000 });
     await expect(page.getByRole("link", { name: newPageTitle })).toBeVisible();
 
-    const pagePromise = page.waitForEvent('popup');
+    const pagePromise = page.waitForEvent("popup");
     await page.getByText(`${NEXT_PUBLIC_SITE_URL}`).click({ force: true });
     const openUrlPage = await pagePromise;
 
     // Wait for the element to become visible or hidden with a longer timeout
-    const sectionCount = await page.locator("div").filter({ hasText: /^No items$/ }).count();
+    const sectionCount = await page
+      .locator("div")
+      .filter({ hasText: /^No items$/ })
+      .count();
     if (sectionCount > 0) {
       // If the section no items is found, expect the Empty Page element to be visible
-      await expect(openUrlPage.getByText("Empty Page")).toBeVisible({ timeout: 20000 }).then(() => console.log('There is no Available Content!'));
+      await expect(openUrlPage.getByText("Empty Page"))
+        .toBeVisible({ timeout: 20000 })
+        .then(() => console.log("There is no Available Content!"));
     }
-  })
-})
+  });
+});
 
 //SEE CURRENT VERSION
 test("See Current Version", async () => {
---
+++ tests/components/sign_in_sign_up.spec.ts
@@ -0,0 +1,297 @@
+import { test, expect, type Page } from "@playwright/test";
+import { NEXT_PUBLIC_SANITY_STUDIO_URL, NEXT_PUBLIC_SITE_URL } from "studio/config";
+import { autologin_studio, createNewPage, deletePageVariant, expectDocumentPublished, navigateToPage } from "tests/utils";
+
+let page: Page;
+
+const internalLinkUrl = `${NEXT_PUBLIC_SITE_URL}/thank-you/`;
+const externalLinkUrl = 'https://facebook.com/'
+const subtitleInput = "Subtitle Input Test";
+const formNameInput = "Form Name Input";
+const signInBtnInput = "Sign In Button";
+const formFields = [
+  {
+    name: "First Name",
+    updatedName: "Name First",
+    placeholder: "First Name",
+    input: "First name",
+  },
+  {
+    name: "Last Name",
+    updatedName: "Name Last",
+    placeholder: "Last Name",
+    input: "Last name",
+  },
+  {
+    name: "Email",
+    updatedName: "Updated Email",
+    placeholder: "Enter your email address",
+    input: "test@gmail.com",
+  },
+  {
+    name: "Password",
+    updatedName: "Updated Password",
+    placeholder: "Enter your password",
+    input: "Test Password",
+  },
+]
+const formLinks = [
+  {
+    name: "Police privacy",
+    updatedName: "Privacy Policy"
+  },
+  {
+    name: "Terms of Use",
+    updatedName: "Use of Terms"
+  },
+]
+let newPageTitle;
+
+test.beforeAll("Auto login studio", async ({ browser }) => {
+  page = await browser.newPage();
+
+  // navigate to the studio
+  await page.goto(`${NEXT_PUBLIC_SANITY_STUDIO_URL}`);
+
+  const token = process.env.NEXT_PUBLIC_STUDIO_AUTOLOGIN_TOKEN_FOR_TESTING;
+  const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+
+  await page.evaluate(autologin_studio, { token, projectId });
+});
+
+async function createSignInSignUpVariant(pageTitle, variantLabel, variantIndex, isInternalLink, linkNames) {
+  newPageTitle = `${pageTitle} `+ new Date().getTime();
+  await navigateToPage(page);
+  await createNewPage(page, newPageTitle, "Sign In Sign Up");
+
+  //Variant
+  if (variantIndex <= 0) {
+    await page.getByTestId("field-variant").getByRole("img").first().click({ force: true });
+  } else {
+    await page.getByTestId("field-variant").getByRole("img").nth(variantIndex).click({ force: true });
+  }
+
+  //Variant Title
+  await page.getByTestId("field-label").getByTestId("string-input").click();
+  await page.getByTestId("field-label").getByTestId("string-input").fill(variantLabel);
+
+  if (isInternalLink) {
+    await page.getByTestId('field-variants.logo.linkType').getByText('Internal, inside this website').click();
+    await page.getByTestId('fieldset-link').getByTestId('autocomplete').click();
+    await page.getByTestId('fieldset-link').getByTestId('autocomplete').fill('thank you');
+    await page.getByRole('button', { name: 'Thank you Published No' }).click();
+    await page.getByText('Self (default) - open in the').click();
+  } else {
+    await page.getByTestId('field-variants.logo.linkType').getByText('External, outside this website').click();
+    await page.getByTestId('field-variants.logo.linkExternal').getByLabel('URL').click();
+    await page.getByTestId('field-variants.logo.linkExternal').getByLabel('URL').fill(externalLinkUrl);
+    await page.getByText('Blank - open on a new tab (').click();
+  }
+
+  //Generate Form ID
+  const formId = "44366aa8-aaf9-45b9-aa3a-610c54ffe766";
+
+  await page.getByLabel('Form ID').click();
+  await page.getByLabel('Form ID').fill(formId);
+  // await page.getByRole('button', { name: 'Generate ID' }).click({ force: true });
+  // expect(page.getByLabel("Form ID")).not.toBeUndefined();
+  // await expect(page.getByRole("button", { name: "Generate ID" })).toBeVisible();
+  // await expect(page.getByRole("link", { name: "Manage" })).toBeVisible();
+  
+  //Subtitle
+  await page.getByTestId('field-variants.form.subtitle').getByTestId('string-input').click();
+  await page.getByTestId('field-variants.form.subtitle').getByTestId('string-input').fill(subtitleInput);
+
+  //Form Name
+  await page.getByTestId('field-variants.form.name').getByTestId('string-input').click();
+  await page.getByTestId('field-variants.form.name').getByTestId('string-input').fill(formNameInput);
+
+  //Fields
+
+  for (const forms of formFields) {
+    await page.getByRole('button', { name: forms.name }).click();
+    await expect(page.getByLabel('Edit WebriQ Form Field')).toBeVisible();
+
+    //Form Name
+    await page.locator(`input[value^="${forms.name}"]`).first().click();
+    await page.locator(`input[value^="${forms.name}"]`).first().fill(forms.updatedName);
+    
+    //Form Placeholder
+    await page.locator(`input[value^="${forms.placeholder}"]`).click();
+    await page.locator(`input[value^="${forms.placeholder}"]`).fill(forms.updatedName);
+
+    //Required field
+    await page.getByLabel('Is this field Required?').click();
+    await page.getByLabel('Close dialog').click();
+  }
+
+  //Submit Button Label
+  const submitBtnInput = "Submit Button"
+  await page.getByTestId('field-variants.form.buttonLabel').getByTestId('string-input').click();
+  await page.getByTestId('field-variants.form.buttonLabel').getByTestId('string-input').fill(submitBtnInput);
+  
+  //Thank you Page Internal/External 
+  await page.getByRole('button', { name: 'Thank You page' }).click();
+  if (!isInternalLink) {
+    await page.getByTestId('field-variants.form.thankYouPage.linkType').getByText('External, outside this website').click();
+    await page.getByTestId('field-variants.form.thankYouPage.linkExternal').getByLabel('URL').click();
+    await page.getByTestId('field-variants.form.thankYouPage.linkExternal').getByLabel('URL').fill(externalLinkUrl);
+    // await page.getByTestId('field-variants.form.thankYouPage.linkTarget').getByText('Blank - open on a new tab (').click();
+  }
+
+  //SignIn Button Link
+  await page.getByTestId('field-variants.signinLink.label').getByTestId('string-input').click();
+  await page.getByTestId('field-variants.signinLink.label').getByTestId('string-input').fill(signInBtnInput);
+
+  if (isInternalLink) {
+    await page.getByTestId('field-variants.signinLink.linkType').getByText('Internal, inside this website').click();
+    await page.getByTestId('autocomplete').click();
+    await page.getByTestId('autocomplete').fill('thank you');
+    await page.getByRole('button', { name: 'Thank you Published No' }).click();
+    await page.getByTestId('field-variants.signinLink.linkTarget').getByText('Self (default) - open in the').click();
+  } else {
+    await page.getByTestId('field-variants.signinLink.linkType').getByText('External, outside this website').click();
+    await page.getByTestId('field-variants.signinLink.linkExternal').getByLabel('URL').click();
+    await page.getByTestId('field-variants.signinLink.linkExternal').getByLabel('URL').fill(externalLinkUrl);
+    await page.getByTestId('field-variants.signinLink.linkTarget').getByText('Blank - open on a new tab (').click();
+  }
+
+  const formLinks = [
+    {
+        name: "Police privacy",
+        updatedName: "Privacy Policy"
+    },
+    {
+        name: "Terms of Use",
+        updatedName: "Use of Terms"
+    },
+  ]
+
+  const routesExternalLink = await page.locator('.sc-jdUcAg > div:nth-child(2) > div label[data-ui="Flex"] span:has-text("External, outside this website"):nth-child(1)');
+  const routesInternalLink = await page.locator('.sc-jdUcAg > div:nth-child(2) > div label[data-as="label"] span:has-text("Internal, inside this website")');
+  
+  const blankLinkTarget = { element: page.locator('.sc-jdUcAg > div:nth-child(2) > div label[data-as="label"] span:has-text("Blank - open on a new tab (")'), target: 'Blank - open on a new tab (' };
+
+  //Form Links
+  for (const links of formLinks) {
+    await page.getByRole('button', { name: links.name }).click();
+    await expect(page.getByLabel('Edit Link')).toBeVisible();
+    
+    await page.locator(`input[value^="${links.name}"]`).click();
+    await page.locator(`input[value^="${links.name}"]`).fill(links.updatedName);
+
+    if (isInternalLink) {
+        await routesInternalLink.click();
+        await page.getByTestId('autocomplete').click({ force: true });
+        await page.getByTestId('autocomplete').fill('thank you');
+        await page.getByRole('button', { name: 'Thank you Published No' }).click();
+    } else {
+        await routesExternalLink.click();
+        await page.locator('.sc-jdUcAg > div:nth-child(2) > div input[inputmode="url"]').click();
+        await page.locator('.sc-jdUcAg > div:nth-child(2) > div input[inputmode="url"]').fill(externalLinkUrl);
+        await blankLinkTarget.element.click();
+    }
+    await page.getByLabel('Close dialog').click();
+  }
+
+  await expectDocumentPublished(page);
+  await expect(page.getByRole('link', { name: newPageTitle })).toBeVisible();
+  
+  const pagePromise = page.waitForEvent('popup');
+  await page.getByText(`${NEXT_PUBLIC_SITE_URL}`).click({ force: true });
+  const openUrlPage = await pagePromise;
+
+  //Fill up form
+  for (const input of formFields) {
+    await openUrlPage.getByLabel(submitBtnInput).click();
+    await expect(openUrlPage.getByPlaceholder(input.updatedName)).toBeFocused();
+    await openUrlPage.getByPlaceholder(input.updatedName).click();
+    await openUrlPage.getByPlaceholder(input.updatedName).fill(input.input);
+  }
+  await openUrlPage.getByLabel(submitBtnInput).click({ force: true });
+  // await expect(openUrlPage.getByText('Sending form data.')).toBeVisible();
+  // await expect(openUrlPage.getByText('✔ Successfully sent form data')).toBeVisible();
+  // await openUrlPage.waitForLoadState('networkidle');
+  // await expect(openUrlPage.getByText('Success!')).toBeVisible({ timeout: 20000 })
+  // const expectedUrl = internalLinkUrl.endsWith('/') ? internalLinkUrl : `${internalLinkUrl}/`;
+  // const receivedUrl = openUrlPage.url().endsWith('/') ? openUrlPage.url() : `${openUrlPage.url()}/`;
+  // await expect(receivedUrl).toBe(expectedUrl);
+
+  let logoImg;
+  isInternalLink ? (logoImg = "Go to /thank-you") : (logoImg = "Go to https://facebook.com/");
+  await assertPageContent(openUrlPage, subtitleInput, formNameInput, formFields, formLinks, logoImg, isInternalLink);
+}
+
+async function assertPageContent (openUrlPage, subtitleInput, formNameInput, formFields, formLinks, linkName, isInternalLink) {
+  await expect(openUrlPage.getByText("Empty Page")).toBeHidden({ timeout: 20000 });
+  await expect(openUrlPage.locator('section')).toBeVisible({ timeout: 20000 });
+
+  await expect(openUrlPage.getByLabel(linkName)).toBeVisible();
+  await expect(openUrlPage.getByText(subtitleInput)).toBeVisible();
+  await expect(openUrlPage.getByRole('heading', { name: formNameInput })).toBeVisible();
+  await expect(openUrlPage.getByLabel(signInBtnInput)).toBeVisible();
+
+  for (const input of formFields) {
+    await expect(openUrlPage.getByPlaceholder(input.updatedName)).toBeVisible();
+    for (const links of formLinks) {
+      await expect(openUrlPage.getByLabel(links.updatedName)).toBeVisible();
+    }
+  }
+  
+  if (isInternalLink) {
+    await openUrlPage.getByRole('link', { name: linkName }).click({ force: true })
+    await openUrlPage.waitForLoadState('networkidle');
+    await expect(openUrlPage.getByText('Success!')).toBeVisible({ timeout: 20000 })
+    const expectedUrl = internalLinkUrl.endsWith('/') ? internalLinkUrl : `${internalLinkUrl}/`;
+    const receivedUrl = openUrlPage.url().endsWith('/') ? openUrlPage.url() : `${openUrlPage.url()}/`;
+    await expect(receivedUrl).toBe(expectedUrl);
+  } else if (!isInternalLink) {
+    const page10Promise = openUrlPage.waitForEvent('popup');
+    await openUrlPage.getByRole('link', { name: linkName }).click({ force: true });
+    const page10 = await page10Promise;
+    const normalizedExpectedUrl = externalLinkUrl.replace("https://www.", "https://");
+    const normalizedReceivedUrl = page10.url().replace("https://www.", "https://");
+    await expect(normalizedReceivedUrl).toBe(normalizedExpectedUrl);
+  }
+}
+
+async function createSignInSignUpTest(pageTitle, variantLabel, variantIndex, isInternalLink, linkNames) {
+  await createSignInSignUpVariant(pageTitle, variantLabel, variantIndex, isInternalLink, linkNames);
+
+  for (const linkName of linkNames) {
+    const slug = newPageTitle?.toLowerCase()?.replace(/\s+/g, "-").replace(/-+/g, "-");
+    await page.goto(`${NEXT_PUBLIC_SITE_URL}/${slug}`)
+
+    await assertPageContent(page, subtitleInput, formNameInput, formFields, formLinks, linkName, isInternalLink);
+  }
+}
+
+test.describe("Variant A Workflow", () => {
+  test.describe.configure({ timeout: 900000, mode: "serial" });
+
+  test("Create Sign In Sign Up Variant A", async () => {
+    const linkNames = [signInBtnInput, "Privacy Policy", "Use of Terms"]
+    await createSignInSignUpTest("Sign In Sign Up A", "New Sign In Sign Up Section A", 0, false, linkNames);
+  });
+
+  test("Delete Sign In Sign Up Variant A", async () => {
+    await deletePageVariant(page, newPageTitle);
+  })
+})
+
+test.describe("Variant B Workflow", () => {
+  test.describe.configure({ timeout: 900000, mode: "serial" });
+
+  test("Create Sign In Sign Up Variant B", async () => {
+    const linkNames = [signInBtnInput, "Privacy Policy", "Use of Terms"]
+    await createSignInSignUpTest("Sign In Sign Up B", "New Sign In Sign Up Section B", 1, true, linkNames);
+  });
+
+  test("Delete Sign In Sign Up Variant B", async () => {
+    await deletePageVariant(page, newPageTitle);
+  })
+})
+
+test.afterAll(async () => {
+  await page.close();
+});
