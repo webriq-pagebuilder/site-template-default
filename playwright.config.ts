90eb05b0693f201f1776ff5e789df7468e299da4
rosellerenrqz <enriquezroseller23@gmail.com>
2024-05-05 16:04:04 +0800

    increased timeout on actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,7 +36,7 @@ export default defineConfig({
 
     storageState: authFile,
 
-    actionTimeout: 60_000,
+    actionTimeout: 120_000,
   },
 
   globalSetup: require.resolve("./playwright/global-setup.ts"),
