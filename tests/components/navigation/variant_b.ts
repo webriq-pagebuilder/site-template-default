66be3f2fcf3b6cd0293a6c9a4e1d4de89c8385f3
rosellerenrqz <enriquezroseller23@gmail.com>
2024-05-11 17:23:52 +0800

    updates on navigation variants test

--- tests/components/navigation/variant_b.ts
+++ tests/components/navigation/variant_b.ts
@@ -1,5 +1,9 @@
 import { expect } from "@playwright/test";
-import { expectDocumentPublished, createSlug } from "tests/utils";
+import {
+  expectDocumentPublished,
+  createSlug,
+  addNavigationRoutes,
+} from "tests/utils";
 
 export default async function Variantb({
   pageTitle,
@@ -157,6 +161,10 @@ export default async function Variantb({
 }
 
 async function assertPageContent(page, linkName, isInternalLink) {
+  await page.waitForSelector(`a[aria-label="${linkName}"]`, {
+    visible: true,
+  });
+
   await expect(
     page.locator(`a[aria-label="${linkName}"]`).first()
   ).toBeVisible();
@@ -170,62 +178,4 @@ async function assertPageContent(page, linkName, isInternalLink) {
       page.locator(`a[aria-label="${linkName}"]`).first()
     ).toHaveAttribute("target", "_self");
   }
-}
-
-async function addNavigationRoutes({
-  page,
-  buttonName,
-  commonFieldValues,
-  isInternalLink,
-}) {
-  await expect(page.getByRole("button", { name: buttonName })).toBeVisible();
-  await page.getByRole("button", { name: buttonName }).click();
-  await expect(page.getByLabel("Edit Link")).toBeVisible();
-  await expect(page.locator('span:has-text("Edit Link")')).toBeVisible();
-
-  // NAVIGATION INTERNAL/EXTERNAL SELECTOR
-  const routesExternalLink = await page.locator(
-    'div:nth-child(2) label[data-ui="Flex"] span:has-text("External, outside this website")'
-  );
-  const routesInternalLink = await page.locator(
-    'div:nth-child(2) label[data-ui="Flex"] span:has-text("Internal, inside this website")'
-  );
-
-  const blankLinkTarget = page.locator(
-    'div:nth-child(2) > div label[data-as="label"] span:has-text("Blank - open on a new tab (")'
-  );
-  const selfLinkTarget = page.locator(
-    'div:nth-child(2) > div label[data-as="label"] span:has-text("Self (default) - open in the")'
-  );
-
-  if (!isInternalLink) {
-    await expect(routesExternalLink.nth(1)).not.toBeChecked();
-    await routesExternalLink.nth(1).click({ force: true });
-    await expect(
-      await page.locator('div input[inputmode="url"]').nth(1)
-    ).toBeVisible();
-    await page
-      .locator('div input[inputmode="url"]')
-      .nth(1)
-      .click({ force: true });
-    await page
-      .locator('div input[inputmode="url"]')
-      .nth(1)
-      .fill(commonFieldValues.externalLinkUrl);
-    await expect(blankLinkTarget.nth(1)).toBeVisible();
-    await blankLinkTarget.nth(1).click();
-  } else {
-    await expect(routesInternalLink.nth(1)).toBeChecked();
-    await routesInternalLink.nth(1).click();
-    await page.getByTestId("autocomplete").click();
-    await page.getByTestId("autocomplete").fill("thank you");
-    await expect(
-      page.getByRole("button", { name: "Thank you Published No" })
-    ).toBeVisible();
-    await page.getByRole("button", { name: "Thank you Published No" }).click();
-    await expect(selfLinkTarget.nth(1)).toBeVisible();
-    await selfLinkTarget.nth(1).click();
-  }
-
-  await page.getByLabel("Close dialog").click();
 }
