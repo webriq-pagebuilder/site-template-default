ee3600453a99f05195eb7859f9e6831b0c50b72f
Dorell WebriQ <info@webriq.com>
2024-06-10 09:05:20 +0800

    fix: detection of cstudio is enabled/disabled

--- tests/config.ts
+++ tests/config.ts
@@ -1,2 +1,4 @@
 export const isEcommerceEnabled =
-  !!process.env.NEXT_PUBLIC_SANITY_STUDIO_IN_CSTUDIO || false;
+  (!!process.env.NEXT_PUBLIC_SANITY_STUDIO_IN_CSTUDIO &&
+    process.env.NEXT_PUBLIC_SANITY_STUDIO_IN_CSTUDIO === "true") ||
+  false;
