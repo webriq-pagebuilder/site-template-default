f471e7e2602ab563fd3a846969a5989d9c8fd954
rosellerenrqz <enriquezroseller23@gmail.com>
2024-07-23 13:51:35 +0800

    initial updates for cookies component test

--- tests/components/cookies/variant_a.ts
+++ tests/components/cookies/variant_a.ts
@@ -1,10 +1,18 @@
 import { expect } from "@playwright/test";
 import { cookiesInitialValue } from "@webriq-pagebuilder/sanity-plugin-schema-default";
 import {
+  descriptionField,
   expectDocumentPublished,
   headingField,
   launchPreview,
 } from "tests/utils";
+
+const positionOptions = [
+  "Top Left",
+  "Top Right",
+  "Bottom Right",
+  "Bottom Left",
+];
 
 export default async function VariantA({
   pageTitle,
@@ -19,17 +27,69 @@ export default async function VariantA({
     commonFieldValues,
   });
 
-  //Cookie Policy
+  //Description
+  await descriptionField.checkAndAddValue({
+    page,
+    initialValue: cookiesInitialValue,
+    placeholder: "Lorem ipsum",
+    commonFieldValues,
+  });
+
+  //Enable Analytics
+  await expect(page.getByText("Enable Analytics")).toBeVisible();
+  await expect(page.getByLabel("Enable Analytics")).toBeVisible();
+  await expect(page.getByLabel("Enable Analytics")).toBeEnabled();
+
+  //Site Name
   await expect(
-    page.getByTestId("activate-overlay").locator("div").first()
+    page
+      .getByTestId("field-variants.config.cookiePolicy.siteName")
+      .getByTestId("string-input")
   ).toBeVisible();
-  await page.getByText("Click to activate").first().click({ force: true });
-  await page.getByTestId("text-style--normal").nth(1).click({ force: true });
-  await page.getByText("We use cookies to personalise").click({ force: true });
-  await page.getByText("We use cookies to personalise").press("Meta+a");
   await page
-    .getByText("We use cookies to personalise")
-    .fill(commonFieldValues.cookiePolicy);
+    .getByTestId("field-variants.config.cookiePolicy.siteName")
+    .getByTestId("string-input")
+    .click();
+  await page
+    .getByTestId("field-variants.config.cookiePolicy.siteName")
+    .getByTestId("string-input")
+    .fill(commonFieldValues.siteName);
+
+  //Cookie Policy Page
+  await page
+    .locator("label")
+    .filter({ hasText: "External, outside this website" })
+    .click();
+  await expect(page.getByLabel("URL")).toBeVisible();
+  await page.getByLabel("URL").click();
+  await page.getByLabel("URL").fill("https://webriq.com");
+  await expect(page.getByText("Link Target")).toBeVisible();
+
+  await page
+    .locator("label")
+    .filter({ hasText: "Internal, inside this website" })
+    .click();
+  await expect(page.getByText("Page Reference")).toBeVisible();
+  await expect(page.getByLabel("URL")).toBeHidden();
+  await expect(page.getByText("Link Target")).toBeHidden();
+  await page.getByTestId("autocomplete").click();
+  await page.getByTestId("autocomplete").fill("thank you");
+  await expect(
+    page.getByRole("button", { name: "Thank You Page Published No" })
+  ).toBeVisible();
+  await page
+    .getByRole("button", { name: "Thank You Page Published No" })
+    .click();
+  await expect(page.getByText("Link Target")).toBeVisible();
+
+  //Consent Modal
+  await expect(page.getByText("Consent Modal", { exact: true })).toBeVisible();
+  for (const position of positionOptions) {
+    await expect(
+      page.locator("label").filter({ hasText: position })
+    ).toBeVisible();
+    await page.locator("label").filter({ hasText: position }).click();
+  }
 
   //Accept Button
   await page
@@ -59,6 +119,9 @@ export default async function VariantA({
   //Heading
   await headingField.sitePreview({ pageUrl: page, commonFieldValues });
 
+  //Description
+  await descriptionField.sitePreview({ pageUrl: page, commonFieldValues });
+
   //Cookie Policy
   await expect(page.getByText(commonFieldValues.cookiePolicy)).toBeVisible();
 
@@ -68,14 +131,16 @@ export default async function VariantA({
   //Decline Button
   await expect(page.getByLabel(commonFieldValues.declineButton)).toBeVisible();
 
+  //Manage
+  await expect(page.getByLabel("Manage individual preferences")).toBeVisible();
+
   //Accept Cookie
   await page.getByLabel(commonFieldValues.acceptButton).click();
+  await page.reload();
 
   //Application Cookies Storage
   const cookies = await page.context().cookies();
-  const cookieConsent = cookies.find(
-    (cookie) => cookie.name === "dxpstudio-cookieconsent"
-  );
+  const cookieConsent = cookies.find((cookie) => cookie.name === "cc_cookie");
   expect(cookieConsent).not.toBeNull(); // Ensure the cookie exists
   expect(cookieConsent.value).toBe("allow");
 }
