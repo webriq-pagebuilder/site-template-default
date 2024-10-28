ac7f8fb2d4955e6c7bfbbc0fe5aeeab6fc8bb15f
MarielF WebriQ <info@webriq.com>
2024-10-28 17:21:40 +0800

    Update preview content if no home page found

--- components/PreviewNoHomePage.tsx
+++ components/PreviewNoHomePage.tsx
@@ -1,12 +1,15 @@
 export function PreviewNoHomePage() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <span className="inline-block px-2 py-1 text-xs font-semibold tracking-widest text-white uppercase bg-blue-600 rounded-full">
+    <div className="flex flex-col items-center justify-center h-screen gap-y-5">
+      <span className="inline-block px-2 py-1 text-xs font-semibold tracking-widest text-white uppercase bg-primary rounded-full">
         Preview Mode
       </span>
       <h1 className="text-2xl font-bold text-gray-900 mt-2">Home Page Not Found</h1>
       <p className="text-gray-600">
-        You are seeing this preview because no home page was found. In order to preview your site or its theme settings, you need to create a home page first.
+        In order to preview your site or its theme settings, start by creating a home page.
+      </p>
+      <p className="text-gray-600">
+        Add content to your site by launching the Studio from the <a className="font-bold text-primary" href="https://stackshift.webriq.com">StackShift App</a>.
       </p>
     </div>
   );
